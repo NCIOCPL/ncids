6f8d86602e3a81c2edbb0dd119f19544da06d39a
karlikpj <paul.karlik@nih.gov>
2021-09-24 08:02:59 -0400

    (#246) Color Tokens Documentation Pages
    * Linting on MDX files
    * Adding test for component
    * Fix incorrect color

---
+++ docs/src/components/ColorChip.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+export const ColorChip = ({ color }) => {
+	const size = 24;
+	const inlineStyle = {
+		background: color || '#000',
+		borderRadius: '6px',
+		width: `${size}px`,
+		height: `${size}px`,
+	};
+	return <div style={inlineStyle}></div>;
+};
+
+ColorChip.propTypes = {
+	color: PropType.string,
+};
+
+export default ColorChip;
