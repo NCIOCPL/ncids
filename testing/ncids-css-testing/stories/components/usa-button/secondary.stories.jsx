a78ad44e07e0de8960b8033f7972f05326a3e154
Olivia Tharp <olivia.tharp@nih.gov>
2021-11-05 12:47:40 -0500

    (#257) Button component scss partials

---
+++ testing/ncids-css-testing/stories/components/usa-button/secondary.stories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import css from '!!raw-loader!sass-loader!./usa-button.scss';
+
+const html = `
+<button class="usa-button usa-button--secondary">Button</button>
+<button class="usa-button usa-button--secondary usa-button--hover">Hover</button>
+<button class="usa-button usa-button--secondary usa-button--active">Active</button>
+<button class="usa-button usa-button--secondary usa-focus">Focus</button>
+<button class="usa-button usa-button--secondary" disabled>Disabled</button>
+`;
+
+export const Secondary = () => <TestCase css={css} html={html} />;
---
+++ testing/ncids-css-testing/stories/components/usa-button/usa-button.stories.jsx
@@ -0,0 +1,14 @@
+export default {
+	title: "components/usa-button",
+	argTypes: {},
+};
+
+export * from "./button.stories";
+export * from "./accent-cool.stories";
+export * from "./accent-warm.stories";
+export * from "./base.stories";
+export * from "./big.stories";
+export * from "./outline.stories";
+export * from "./outline-inverse.stories";
+export * from "./secondary.stories";
+export * from "./unstyled.stories";
