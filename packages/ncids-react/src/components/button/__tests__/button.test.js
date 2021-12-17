36cf35b60d5c2f896aafbe6d96e53fa380cb52de
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2021-12-17 16:03:26 -0500

    (#289) Fixed ncids-react linter rules
    - Also fixed issues with the linter. (Lots of double quotes)
    - Updated setup-node action to authenticate to github registry

--- packages/ncids-react/src/components/button/__tests__/button.test.js
+++ packages/ncids-react/src/components/button/__tests__/button.test.js
@@ -1,74 +1,73 @@
-import React from "react";
-import { cleanup, render, fireEvent } from "@testing-library/react";
-import Button from "../button";
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import Button from '../button';
 
-afterEach(cleanup);
-
-describe("Button component", () => {
-	it("should be defined", () => {
+describe('Button component', () => {
+	it('should be defined', () => {
 		expect(Button).toBeDefined();
 	});
 
-	it("renders a uswds button by default", () => {
-		const btn = render(<Button label="test" />);
-		expect(btn).toMatchSnapshot();
+	it('renders a uswds button by default', () => {
+		const { asFragment } = render(<Button label="test" />);
+		expect(asFragment()).toMatchSnapshot();
 	});
 
-	it("renders label through as button content", () => {
-		const mockChildText = "chicken";
-		const { queryByText } = render(<Button label={mockChildText} />);
-		expect(queryByText("chicken")).toBeTruthy();
+	it('renders label through as button content', () => {
+		const mockChildText = 'chicken';
+		render(<Button label={mockChildText} />);
+		expect(screen.getByRole('button')).toHaveTextContent('chicken');
 	});
 
-	it("renders a button of proper type when specified", () => {
-		const { getByText } = render(<Button type="submit" label="Submit" />);
-		expect(getByText(/submit/i)).toHaveAttribute("type", "submit");
+	it('renders a button of proper type when specified', () => {
+		render(<Button type="submit" label="Submit" />);
+		expect(screen.getByText(/submit/i)).toHaveAttribute('type', 'submit');
 	});
 
 	it("renders a 'secondary' variant", () => {
-		const { getByText } = render(<Button label="test" variant="secondary" />);
-		expect(getByText(/test/i)).toHaveClass("usa-button--secondary");
+		render(<Button label="test" variant="secondary" />);
+		expect(screen.getByText(/test/i)).toHaveClass('usa-button--secondary');
 	});
 
 	it("renders a 'outline' variant", () => {
-		const { getByText } = render(<Button label="test" variant="outline" />);
-		expect(getByText(/test/i)).toHaveClass("usa-button--outline");
+		render(<Button label="test" variant="outline" />);
+		expect(screen.getByRole('button')).toHaveTextContent('test');
+		expect(screen.getByRole('button')).toHaveClass('usa-button--outline');
 	});
 
 	it("renders a 'big' variant", () => {
-		const { getByText } = render(<Button label="test" variant="big" />);
-		expect(getByText(/test/i)).toHaveClass("usa-button--big");
+		render(<Button label="test" variant="big" />);
+		expect(screen.getByText(/test/i)).toHaveClass('usa-button--big');
 	});
 
 	it("renders a 'unstyled' variant", () => {
-		const { getByText } = render(<Button label="test" variant="unstyled" />);
-		expect(getByText(/test/i)).toHaveClass("usa-button--unstyled");
+		render(<Button label="test" variant="unstyled" />);
+		expect(screen.getByText(/test/i)).toHaveClass('usa-button--unstyled');
 	});
 
-	it("renders a disabled button when passed disabled", () => {
-		const { getByText } = render(<Button disabled label="Disabled Button" />);
-		expect(getByText(/disabled button/i)).toBeDisabled();
+	it('renders a disabled button when passed disabled', () => {
+		render(<Button disabled label="Disabled Button" />);
+		expect(screen.getByText(/disabled button/i)).toBeDisabled();
 	});
 
-	it("adds classes when specified", () => {
-		const { getByText } = render(
+	it('adds classes when specified', () => {
+		render(
 			<Button
 				classes="usa-button--outline usa-button--inverse ncids-btn-class"
 				label="myClasses"
 			/>
 		);
-		expect(getByText(/myclasses/i)).toHaveClass("ncids-btn-class");
+		expect(screen.getByText(/myclasses/i)).toHaveClass('ncids-btn-class');
 	});
 
-	it("passes otherProps through when provided", () => {
-		const { queryByText } = render(<Button id="testid" label="test" />);
-		expect(queryByText("test")).toHaveAttribute("id", "testid");
+	it('passes otherProps through when provided', () => {
+		render(<Button id="testid" label="test" />);
+		expect(screen.queryByText('test')).toHaveAttribute('id', 'testid');
 	});
 
 	it('calls "clickAction" function when clicked', () => {
 		const mockFn = jest.fn();
-		const { getByText } = render(<Button onClick={mockFn} label="test" />);
-		fireEvent.click(getByText(/test/));
+		render(<Button onClick={mockFn} label="test" />);
+		fireEvent.click(screen.getByText(/test/));
 		expect(mockFn).toHaveBeenCalled();
 	});
 });
