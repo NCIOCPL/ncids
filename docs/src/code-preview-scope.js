185c0905b18e4f4fe6be0682af5b1e06f9fd3e5a
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2022-01-11 22:07:06 -0500

    (#303) Added in code preview based on example code
    - Updated code component to also display a preview only if:
      A) The language is supported, so only html or jsx
      B) The code block does not have the 'nopreview' attribute
    - Updated multiple documentation pages to remove examples so that
      the code displays the example.
    - Also updated Gatsby to 3.13.1 which allowed for a webpack update
      required for one of the preview modules.

---
+++ docs/src/code-preview-scope.js
@@ -0,0 +1,6 @@
+// This ensures that all ncids react components are available from
+// the code preview.
+import * as ncidsComponents from '@nciocpl/ncids-react';
+export default {
+	...ncidsComponents,
+};
