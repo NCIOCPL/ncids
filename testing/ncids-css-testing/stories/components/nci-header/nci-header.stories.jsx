c96069bf3be61117eb43d863cec00c385810986c
karlikpj <paul.karlik@nih.gov>
2022-01-25 08:14:13 -0500

    (#296) CSS for Extended Header
    - Default Desktop and Primary + Mega
    - Adding Mobile DOM
    - Updating CSS for Mobile
    - PR feedback for file naming
    - Fixing close button
    - Adding Class/Interface for header
    - Fixing test coverage
    - Updating SCSS
    - Rename Classes
    - Update to DOM/Buttons
    - Break out search styles into own file
    - Update Close button icon
    - Adding hover for mobile test
    - Rename scenarios
    - Update Visual Tests
    - Fix color
    - Update to Font Family
    - Adding HTML mock
    - Update buttons to links
    - Adding Header to Doc Site
    - New scenarios for Non JS and updated DOM
    - Fix Docsite Header
    - Removing USA Accordion SCSS -no longer used
    - Merge from develop
    - PR Feedback
    - Remove Tabs from Doc Page
    - Path update for settings
    - New Line
    - Formatting mdx
    - Remove Duplicate SCSS and Stories

---
+++ testing/ncids-css-testing/stories/components/nci-header/nci-header.stories.jsx
@@ -0,0 +1,21 @@
+export default {
+	title: 'components/nci-header',
+	argTypes: {},
+};
+
+
+export * from './nci-extended-desktop-nojs.stories';
+export * from './nci-extended-desktop-nojs-hover.stories';
+export * from './nci-extended-desktop-default.stories';
+export * from './nci-extended-desktop-primary-hover.stories';
+export * from './nci-extended-desktop-primary-active.stories';
+export * from './nci-extended-desktop-megamenu-expanded.stories';
+export * from './nci-extended-desktop-sws.stories';
+
+export * from './nci-extended-mobile-default.stories';
+export * from './nci-extended-mobile-first-level.stories';
+export * from './nci-extended-mobile-hover.stories';
+export * from './nci-extended-mobile-second-level.stories';
+export * from './nci-extended-mobile-active-section.stories';
+export * from './nci-extended-mobile-active-section-root.stories';
+export * from './nci-extended-mobile-sws.stories';
