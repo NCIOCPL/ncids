503342c01709f2a3e3519f2991b098298c984673
karlikpj <paul.karlik@nih.gov>
2022-01-25 08:14:13 -0500

    (#296) CSS for Extended Header
    - Default Desktop and Primary + Mega
    - Adding Mobile DOM
    - Updating CSS for Mobile
    - PR feedback for file naming
    - Fixing close button
    - Adding Class/Interface for header
    - Fixing test coverage
    - Updating SCSS
    - Rename Classes
    - Update to DOM/Buttons
    - Break out search styles into own file
    - Update Close button icon
    - Adding hover for mobile test
    - Rename scenarios
    - Update Visual Tests
    - Fix color
    - Update to Font Family
    - Adding HTML mock
    - Update buttons to links
    - Adding Header to Doc Site
    - New scenarios for Non JS and updated DOM
    - Fix Docsite Header
    - Removing USA Accordion SCSS -no longer used
    - Merge from develop
    - PR Feedback
    - Remove Tabs from Doc Page
    - Path update for settings
    - New Line
    - Formatting mdx
    - Remove Duplicate SCSS and Stories
    - Merge from Dev

--- packages/ncids-js/src/index.ts
+++ packages/ncids-js/src/index.ts
@@ -2,3 +2,4 @@
 export { NCIBigFooter } from './components/usa-footer/nci-big-footer.component';
 export { NCISiteAlert } from './components/usa-site-alert/nci-site-alert.component';
 export { NCISubscribe } from './components/nci-subscribe/nci-subscribe.component';
+export { NCIExtendedHeaderWithMegaMenu } from './components/usa-header/nci-header.component';
