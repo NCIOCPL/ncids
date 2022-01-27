8212ef7b1803a8dd0de4cfd9bd4fa602b211d3e8
Olivia Tharp <olivia.tharp@nih.gov>
2022-01-27 15:27:42 -0500

    (#261) Build banner component

--- docs/src/components/banner.jsx
+++ docs/src/components/banner.jsx
@@ -1,77 +1,17 @@
 import React from 'react';
-import { withPrefix } from 'gatsby';
 
 const Banner = () => (
-	<div className="usa-banner">
-		<div className="usa-accordion">
-			<header className="usa-banner__header">
-				<div className="usa-banner__inner">
-					<div className="grid-col-auto">
-						<img
-							className="usa-banner__header-flag"
-							src={withPrefix('/us_flag_small.png')}
-							alt="U.S. flag"
-						/>
-					</div>
-					<div className="grid-col-fill tablet:grid-col-auto">
-						<p className="usa-banner__header-text">
-							An official website of the United States government
-						</p>
-						<p className="usa-banner__header-action" aria-hidden="true">
-							Here’s how you know
-						</p>
-					</div>
-					<button
-						aria-controls="gov-banner"
-						aria-expanded={false}
-						className="usa-accordion__button usa-banner__button">
-						<span className="usa-banner__button-text">
-							Here&apos;s how you know
-						</span>
-					</button>
-				</div>
-			</header>
-			<div
-				id="gov-banner"
-				className="usa-accordion__content usa-banner__content"
-				hidden>
-				<div className="grid-row grid-gap-lg">
-					<div className="usa-banner__guidance-gov tablet:grid-col-6">
-						<img
-							className="usa-banner__icon usa-media-block__img"
-							src={withPrefix('icon-dot-gov.svg')}
-							alt="Dot gov"
-						/>
-						<div className="usa-media-block__body">
-							<p>
-								<strong>The .gov means it’s official.</strong>
-								<br />
-								Federal government websites often end in .gov or .mil. Before
-								sharing sensitive information, make sure you’re on a federal
-								government site.
-							</p>
-						</div>
-					</div>
-					<div className="usa-banner__guidance-ssl tablet:grid-col-6">
-						<img
-							className="usa-banner__icon usa-media-block__img"
-							src={withPrefix('icon-https.svg')}
-							alt="Https"
-						/>
-						<div className="usa-media-block__body">
-							<p>
-								<strong>The site is secure.</strong>
-								<br />
-								The <strong>https://</strong> ensures that you are connecting to
-								the official website and that any information you provide is
-								encrypted and transmitted securely.
-							</p>
-						</div>
-					</div>
+	<section
+		className="usa-banner usa-banner--nci-banner"
+		aria-label="Official government website">
+		<header className="usa-banner__header">
+			<div className="usa-banner__inner">
+				<div className="usa-banner__header-text">
+					An official website of the United States government
 				</div>
 			</div>
-		</div>
-	</div>
+		</header>
+	</section>
 );
 
 export default Banner;
