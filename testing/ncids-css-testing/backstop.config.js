e49b0f2becfc88d26db15e703616a61a55650582
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2022-02-07 17:47:44 -0500

    (#328) Support visual regression testing for focus
    - Updated BackstopJS and changed the engine to be playwright
    - Added a `focusSelector` scenario option to engine scripts
    - Updated reference images that changed with the new driver, including
       a number of for-the-better images. (e.g. Buttons were all missing
       their rounded outlines on focus/active)

--- testing/ncids-css-testing/backstop.config.js
+++ testing/ncids-css-testing/backstop.config.js
@@ -2,15 +2,15 @@ const glob = require('./util/glob_hack');
 const pathUtil = require('path');
 
 const path =
-	process.env.CI === "true"
-		? "http://localhost:6006/"
-		: "http://host.docker.internal:6006/";
+	process.env.CI === 'true'
+		? 'http://localhost:6006/'
+		: 'http://host.docker.internal:6006/';
 
 // Copying environment variables to docker container
-const additionalVars = ["CI"].reduce(
+const additionalVars = ['CI'].reduce(
 	(ac, varName) =>
 		process.env[varName] ? `${ac}-e ${varName}='${process.env[varName]}' ` : ac,
-	" "
+	' '
 );
 const dockerCommandTemplate = `docker run --rm -i --net=host ${additionalVars} --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}`;
 
@@ -21,53 +21,51 @@ const scenarioFiles = glob.sync('stories/**/*.scenarios.js');
 const scenariosExpanded = scenarioFiles.reduce((ac, scenarioFile) => {
 	const scenarios = require(pathUtil.resolve(__dirname, scenarioFile));
 	const cleanedScenarios = scenarios.map((scenario) => ({
-					...scenario,
-					url: `${path}iframe.html?id=${scenario.storyId}&args=&viewMode=story`,
+		...scenario,
+		url: `${path}iframe.html?id=${scenario.storyId}&args=&viewMode=story`,
 	}));
 	return [...ac, ...cleanedScenarios];
-},[]);
-
+}, []);
 
 module.exports = {
-	id: "ncids",
+	id: 'ncids',
 	viewports: [
 		{
-			label: "mobile",
+			label: 'mobile',
 			width: 479,
 			height: 360,
 		},
 		{
-			label: "mobile-lg",
+			label: 'mobile-lg',
 			width: 480,
 			height: 360,
 		},
 		{
-			label: "tablet",
+			label: 'tablet',
 			width: 640,
 			height: 360,
 		},
 		{
-			label: "desktop",
+			label: 'desktop',
 			width: 1024,
 			height: 768,
 		},
 	],
-	onBeforeScript: "puppet/onBefore.js",
-	onReadyScript: "puppet/onReady.js",
-	scenarios: [
-		...scenariosExpanded,
-	],
+	onBeforeScript: 'playwright/onBefore.js',
+	onReadyScript: 'playwright/onReady.js',
+	scenarios: [...scenariosExpanded],
 	paths: {
-		bitmaps_reference: ".backstop/reference",
-		bitmaps_test: ".backstop/test",
-		engine_scripts: ".backstop/engine-scripts",
-		html_report: ".backstop/html-report",
-		ci_report: ".backstop/ci-report",
+		bitmaps_reference: '.backstop/reference',
+		bitmaps_test: '.backstop/test',
+		engine_scripts: '.backstop/engine-scripts',
+		html_report: '.backstop/html-report',
+		ci_report: '.backstop/ci-report',
 	},
-	report: ["browser"],
-	engine: "puppeteer",
+	report: ['browser'],
+	engine: 'playwright',
 	engineOptions: {
-		args: ["--no-sandbox"],
+		browser: 'chromium',
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
 	},
 	asyncCaptureLimit: 5,
 	asyncCompareLimit: 50,
