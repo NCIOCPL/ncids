68f4bdc9a5134cc3be92a9edc0d238b2f1420ab5
Olivia Tharp <olivia.tharp@nih.gov>
2022-04-01 12:51:10 -0500

    (#312) usa-sidenav - updated guidance text

---
+++ testing/ncids-css-testing/stories/components/usa-sidenav/nci-sidenav-level3.stories.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import css from './nci-sidenav.scss';
+
+const html = `
+<div class='grid-row grid-gap'>
+  <div class='grid-col-4'>
+    <nav aria-label='Secondary navigation example'>
+      <ul class='usa-sidenav usa-sidenav--nci-sidenav'>
+        <li class='usa-sidenav__item'>
+          <a href='#'>Level 1</a>
+        </li>
+        <li class='usa-sidenav__item'>
+          <a href='#'>Level 1</a>
+        </li>
+        <li class='usa-sidenav__item'>
+          <a href='#' class='usa-current usa-current--nci-ancestor backstop-only--level-1'>Level 1</a>
+          <ul class='usa-sidenav__sublist'>
+            <li class='usa-sidenav__item'>
+              <a href='#''>Level 2</a>
+            </li>
+            <li class='usa-sidenav__item'>
+              <a href='#'>Level 2</a>
+            </li>
+            <li class='usa-sidenav__item'>
+              <a href='#' class='usa-current usa-current--nci-ancestor backstop-only--level-2'>Level 2</a>
+              <ul class='usa-sidenav__sublist'>
+								<li class='usa-sidenav__item'>
+									<a href='#'>Level 3</a>
+								</li>
+								<li class='usa-sidenav__item'>
+									<a href='#' class='usa-current'>Level 3</a>
+								</li>
+								<li class='usa-sidenav__item'>
+									<a href='#' class='nci-has-children'>Level 3</a>
+								</li>
+							</ul>
+            </li>
+					</ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+`;
+
+export const NCISidenavLevel3 = () => <TestCase css={css} html={html} />;
---
+++ testing/ncids-css-testing/stories/components/usa-sidenav/usa-sidenav.stories.jsx
@@ -0,0 +1,9 @@
+export default {
+	title: 'components/usa-sidenav',
+	argTypes: {},
+};
+
+export * from './nci-sidenav.stories';
+export * from './nci-sidenav-level1.stories';
+export * from './nci-sidenav-level2.stories';
+export * from './nci-sidenav-level3.stories';
