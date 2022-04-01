3c0bf778f43ef00eb3a11c740237e650ef8fa1d9
Olivia Tharp <olivia.tharp@nih.gov>
2022-04-01 12:51:10 -0500

    (#312) usa-sidenav - updated guidance text

---
+++ testing/ncids-css-testing/stories/components/usa-sidenav/nci-sidenav-level2.stories.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import css from './nci-sidenav.scss';
+
+const html = `
+<div class='grid-row grid-gap'>
+  <div class='grid-col-4'>
+    <nav aria-label='Secondary navigation example'>
+      <ul class='usa-sidenav usa-sidenav--nci-sidenav'>
+        <li class='usa-sidenav__item'>
+          <a href='#'>Level 1</a>
+        </li>
+        <li class='usa-sidenav__item'>
+          <a href='#'>Level 1</a>
+        </li>
+        <li class='usa-sidenav__item'>
+          <a href='#' class='usa-current usa-current--nci-ancestor'>Level 1</a>
+          <ul class='usa-sidenav__sublist'>
+            <li class='usa-sidenav__item'>
+              <a href='#' class='backstop-only--level-2'>Level 2</a>
+            </li>
+            <li class='usa-sidenav__item'>
+              <a href='#' class='usa-current'>Level 2</a>
+            </li>
+            <li class='usa-sidenav__item'>
+              <a href='#' class='nci-has-children'>Level 2</a>
+            </li>
+					</ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+`;
+
+export const NCISidenavLevel2 = () => <TestCase css={css} html={html} />;
