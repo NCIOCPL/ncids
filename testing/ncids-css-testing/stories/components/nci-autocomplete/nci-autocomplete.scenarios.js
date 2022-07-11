ef9fbdd339b70c103f846b8745333d920e5300e8
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2022-07-11 12:33:48 -0400

    (#280) nci-autocomplete
    v0.1.0-alpha.60

---
+++ testing/ncids-css-testing/stories/components/nci-autocomplete/nci-autocomplete.scenarios.js
@@ -0,0 +1,26 @@
+module.exports = [
+	{
+		label: 'nci-autocomplete default',
+		storyId: 'components-nci-autocomplete--default',
+	},
+	{
+		label: 'nci-autocomplete input-below-min-threshold',
+		storyId: 'components-nci-autocomplete--below-min',
+	},
+	{
+		label: 'nci-autocomplete input-below-min-message',
+		storyId: 'components-nci-autocomplete--min-msg',
+	},
+	{
+		label: 'nci-autocomplete suggestions',
+		storyId: 'components-nci-autocomplete--suggestions',
+	},
+	{
+		label: 'nci-autocomplete option-highlighted',
+		storyId: 'components-nci-autocomplete--option-highlighted',
+	},
+	{
+		label: 'nci-autocomplete matching-highlight-disabled',
+		storyId: 'components-nci-autocomplete--match-disabled',
+	},
+];
--- testing/ncids-js-testing/src/index.ts
+++ testing/ncids-js-testing/src/index.ts
@@ -1,6 +1,7 @@
 import './index.scss';
 
 import {
+	NCIAutocomplete,
 	NCISiteAlert,
 	NCIExtendedHeaderWithMegaMenu,
 	NCIBigFooter,
@@ -8,6 +9,7 @@ import {
 
 import { MockMegaMenuAdaptor } from './MockMegaMenuAdaptor';
 import { MockMobileMenuAdaptor } from './MockMobileMenuAdaptor';
+import { MockAutocompleteAdaptor } from './MockAutocompleteAdaptor';
 
 window.addEventListener('DOMContentLoaded', () => {
 	console.log('Initializing');
@@ -24,12 +26,47 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	}
 
-	const siteAlerts = document.querySelectorAll('.usa-site-alert');
-	siteAlerts.forEach((element) => {
-		const alert = element as HTMLElement;
-		const closeable = alert.dataset.siteAlertClosable?.toLowerCase() === 'true';
-		NCISiteAlert.create(alert, {
-			closeable: closeable,
+	// set up autocomplete for sitewide search
+	const autocompleteInstance = document.querySelector(
+		'#nci-header-search__field'
+	) as HTMLInputElement;
+	if (autocompleteInstance) {
+		const MockACSource = new MockAutocompleteAdaptor();
+		NCIAutocomplete.create(autocompleteInstance, {
+			autocompleteSource: MockACSource,
+			maxOptionsCount: 10,
+			minCharCount: 3,
+			listboxClasses: 'listboxWidth',
 		});
-	});
+	}
+
+	// Autocomplete plucks the search field off the dom to move it, so we lose the existing event listeners
+	// replace them here
+	const searchField = document.querySelector(
+		'#nci-header-search__field'
+	) as HTMLInputElement;
+	const navSecondary = searchField.closest(
+		'.nci-header-nav__secondary'
+	) as HTMLDivElement;
+	searchField.addEventListener(
+		'focus',
+		() => {
+			navSecondary.classList.add('search-focused');
+		},
+		false
+	);
+	searchField.addEventListener(
+		'focusout',
+		() => {
+			navSecondary.classList.remove('search-focused');
+		},
+		false
+	);
+
+	const covidBanner = document.getElementById('site-alert--nci-info');
+	if (covidBanner) {
+		NCISiteAlert.create(covidBanner, {
+			closeable: true,
+		});
+	}
 });
