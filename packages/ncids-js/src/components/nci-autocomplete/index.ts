77c3a787650d5d0d8ad36efd90ab74501da25843
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2022-07-11 12:33:48 -0400

    (#280) nci-autocomplete
    Co-authored-by: Derek Lescarbeau <dlescarbeau@users.noreply.github.com>
    publish url mismatch
    documentation first pass
    
    v0.1.0-alpha.56

---
+++ packages/ncids-js/src/components/nci-autocomplete/index.ts
@@ -0,0 +1,10 @@
+export { NCIAutocomplete } from './nci-autocomplete.component';
+
+import type { AutocompleteAdaptor } from './utils/autocomplete-adaptor';
+export type { AutocompleteAdaptor };
+
+import type {
+	NCIAutocompleteOptions,
+	AutocompleteOptions,
+} from './utils/autocomplete-options';
+export type { NCIAutocompleteOptions, AutocompleteOptions };
