79bc1b247f1858dfed6ed72ed1c6c5f8af7184f4
Olivia Tharp <olivia.tharp@nih.gov>
2022-09-28 13:43:30 -0500

    (#593) Fix nci header mobile back button so that it passes href or id

--- packages/ncids-js/src/components/nci-header/utils/mobile-menu/mobile-menu-adaptor.ts
+++ packages/ncids-js/src/components/nci-header/utils/mobile-menu/mobile-menu-adaptor.ts
@@ -10,20 +10,28 @@ import { MobileMenuData } from './mobile-menu-data';
  * (i.e. not the entire navigation tree)
  */
 export interface MobileMenuAdaptor {
-	useUrlForNavigationId: boolean;
 	/**
+	 * This defines what should be passed to the adapter for fetching menus. If
+	 * this is true then the HREF of the navigation link will be provided to the
+	 * adapter. If this is false then the header will use the data-menu-id
+	 * attribute of the navigation link instead. It is helpful to use an ID if
+	 * you have some dynamic system which only allows you to fetch menu
+	 * information by ID instead of a path.
+	 * */
+	useUrlForNavigationId: boolean;
+
+	/*
 	 * As users navigate the menu items, the header will request the corresponding menu
 	 * from the adapter. It can do this since it is the one drawing the menu items. However,
 	 * on initial click of the Mobile Menu Button, the header will not know what menu to fetch.
 	 * This method is provided for just that purpose. See Sequence of Events for a Mobile
 	 * Menu Adapter below for more information.
-	 *
 	 */
 	getInitialMenuId(): Promise<string | number>;
 
 	/**
 	 * This method returns a level of navigation to be displayed in the mobile menu.
-	 * This should include the "Explore ABC
+	 * This should include the "Explore ABC".
 	 *
 	 * @param id mobile menu `data-menu-id`
 	 */
