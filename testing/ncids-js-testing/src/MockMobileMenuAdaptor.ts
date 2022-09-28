12c4f23dea53660eba4d207e306435fd28d2172c
Olivia Tharp <olivia.tharp@nih.gov>
2022-09-28 13:57:15 -0500

    (#633) Fix nci header mobile menu items to use href or id

--- testing/ncids-js-testing/src/MockMobileMenuAdaptor.ts
+++ testing/ncids-js-testing/src/MockMobileMenuAdaptor.ts
@@ -12,25 +12,39 @@ export class MockMobileMenuAdaptor implements MobileMenuAdaptor {
 		return this.useUrlForNavigationId ? '/root-menu' : 999;
 	}
 
-	async getNavigationLevel(id: string): Promise<MobileMenuData> {
+	async getNavigationLevel(id: number | string): Promise<MobileMenuData> {
+		if (!id) {
+			throw new Error(`ID not found`);
+		}
+
 		const rootPath = window.location.origin;
-		const path = id === '/' ? '/root-menu' : id;
 
-		return await fetch(`${rootPath}/data/mobile-menu${path}.json`)
-			.then((response) => response.json())
-			.catch((error) => {
-				console.error(`Fetch Error: ${error}`);
+		if (this.useUrlForNavigationId) {
+			const path = id === '/' ? '/root-menu' : id;
+			return this.fetchData(`${rootPath}/data/mobile-menu${path}.json`);
+		} else {
+			/*
+			 * Workaround for testing !this.useUrlForNavigationId
+			 * Set mobileMenuSource: new MockMobileMenuAdaptor(false), to trigger
+			 */
+			let path = '/root-menu';
+			if (id === '1') {
+				path = '/section-one';
+			} else if (id === '2') {
+				path = '/section-two';
+			}
 
-				// todo adaptor only allows @return Promise<MobileMenuData>
-				return {
-					id: 0,
-					label: 'Homepage',
-					path: '/',
-					langcode: 'en',
-					hasChildren: false,
-					items: [],
-					parent: null,
-				};
-			});
+			return this.fetchData(`${rootPath}/data/mobile-menu${path}.json`);
+		}
+	}
+
+	async fetchData(path: string): Promise<MobileMenuData> {
+		return await fetch(path).then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+
+			return response.json();
+		});
 	}
 }
