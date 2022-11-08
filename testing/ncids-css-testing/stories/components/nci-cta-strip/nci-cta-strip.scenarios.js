b464c4b304c3248db3b5e097fa1cc8e7eaa54709
karlikpj <paul.karlik@nih.gov>
2022-11-08 11:12:47 -0500

    (#548) Hero Cta Strip

---
+++ testing/ncids-css-testing/stories/components/nci-cta-strip/nci-cta-strip.scenarios.js
@@ -0,0 +1,95 @@
+module.exports = [
+	{
+		label: 'nci-cta-strip default',
+		storyId: 'components-nci-cta-strip--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-cta-strip default long',
+		storyId: 'components-nci-cta-strip--default-long',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-cta-strip default spanish',
+		storyId: 'components-nci-cta-strip--default-spanish',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+];
