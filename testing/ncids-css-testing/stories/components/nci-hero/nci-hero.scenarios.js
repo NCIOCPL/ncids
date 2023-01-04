a532932dfcef4b715f95b13534d0bb72a239b6f3
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-01-04 15:19:46 -0500

    (#535) nci-hero class added, example on cgov home

---
+++ testing/ncids-css-testing/stories/components/nci-hero/nci-hero.scenarios.js
@@ -0,0 +1,33 @@
+module.exports = [
+	{
+		label: 'nci-hero default',
+		storyId: 'components-nci-hero--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 880,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+];
--- testing/ncids-css-testing/stories/templates/cgov-home/default.stories.jsx
+++ testing/ncids-css-testing/stories/templates/cgov-home/default.stories.jsx
@@ -4,6 +4,13 @@ import css from './cgov-home.scss';
 
 import { nciImgLogo } from '../../components/nci-header/nci-header-logo';
 
+import img_hero_mobile from './img/hero-mobile.jpg';
+import img_hero_mobile_lg from './img/hero-mobile-large.jpg';
+import img_hero_tablet from './img/hero-tablet.jpg';
+import img_hero_tablet_lg from './img/hero-tablet-large.jpg';
+import img_hero_desktop from './img/hero-desktop.jpg';
+import img_hero_widerscreen from './img/hero-widescreen.jpg';
+
 import img_promo_16x9 from './img/promo-16x9.jpg';
 import img_promo_1x1 from './img/promo-1x1.jpg';
 
@@ -97,8 +104,23 @@ const html = `
 		</nav>
 	</header>
 
-	<section class="usa-section" style="background-color: #f0f0f0">
-		<div style="height: 100px; background-color: white; border: 1px dashed black">Page Hero</div>
+	<section style="background-color: #f0f0f0">
+		<div class="nci-hero text-center">
+			<picture class="nci-hero__image">
+				<source media="(min-width: 1024px)" srcset="${img_hero_widerscreen}" />
+				<source media="(min-width: 880px)" srcset="${img_hero_desktop}" />
+				<source media="(min-width: 640px)" srcset="${img_hero_tablet_lg}" />
+				<source media="(min-width: 480px)" srcset="${img_hero_tablet}" />
+				<source media="(min-width: 320px)" srcset="${img_hero_mobile_lg}" />
+				<img src="${img_hero_mobile}" alt="" />
+			</picture>
+			<div class="nci-hero__cta-container">
+				<div class="nci-hero__cta">
+					<h2 class="nci-hero__cta-tagline">NCI is the nation's leader in cancer research.</h2>
+					<a href="https://www.cancer.gov" class="nci-hero__cta-button usa-button usa-button--big">Learn More</a>
+					</div>
+			</div>
+		</div>
 	</section>
 
 	<section class="usa-section" style="background-color: #f0f0f0">
