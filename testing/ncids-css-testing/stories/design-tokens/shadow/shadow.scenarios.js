430e9e187283e8888e0aa6007615950b7434eade
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-01-09 11:13:33 -0500

    (#603) Updates USWDS drop shadow to 30% from 10%

---
+++ testing/ncids-css-testing/stories/design-tokens/shadow/shadow.scenarios.js
@@ -0,0 +1,13 @@
+module.exports = [
+	{
+		label: 'Box Shadow Examples',
+		storyId: 'design-tokens-shadow--default',
+		viewports: [
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	}
+];
