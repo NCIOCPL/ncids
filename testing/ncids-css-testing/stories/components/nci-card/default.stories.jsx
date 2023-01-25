02e1a38e626598aa997ade5863727113b137b1b9
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-01-25 13:11:03 -0500

    (#552) Adds Feature Card Component

---
+++ testing/ncids-css-testing/stories/components/nci-card/default.stories.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import css from './nci-card.scss';
+
+import img_news1_16x9 from './img/news1-16x9.jpg';
+import img_news1_1x1 from './img/news1-1x1.jpg';
+import img_news1_4x3 from './img/news1-4x3.jpg';
+
+import img_news2_16x9 from './img/news2-16x9.jpg';
+import img_news2_1x1 from './img/news2-1x1.jpg';
+import img_news2_4x3 from './img/news2-4x3.jpg';
+
+import img_news3_16x9 from './img/news3-16x9.jpg';
+import img_news3_1x1 from './img/news3-1x1.jpg';
+import img_news3_4x3 from './img/news3-4x3.jpg';
+
+// language=HTML
+const html = `
+  <div id="nci-card" class="grid-container">
+    <div class="nci-card">
+      <a href="https://www.cancer.gov" aria-label="Feature Card">
+        <picture class="nci-card__image">
+          <source
+            media="(min-width: 1024px)"
+            srcset="${img_news1_4x3}"
+          />
+          <img src="${img_news1_16x9}" />
+        </picture>
+        <div class="nci-card__body">
+          <span class="nci-card__title">CRCHD Diversity Training</span>
+          <p class="nci-card__description">
+            Treatment for severe COVID-19 with interferons decreased the
+            viral load of SARS-CoV-2, a new study found.
+          </p>
+        </div>
+      </a>
+    </div>
+  </div>
+`;
+
+export const Default = () => <TestCase css={css} html={html} />;
