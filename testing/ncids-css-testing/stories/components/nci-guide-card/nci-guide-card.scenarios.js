e1f71e505562204b885869888f56e821b115ff0c
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-02-16 13:28:41 -0500

    (#614) Adds Guide Card with Image and Description

--- testing/ncids-css-testing/stories/components/nci-guide-card/nci-guide-card.scenarios.js
+++ testing/ncids-css-testing/stories/components/nci-guide-card/nci-guide-card.scenarios.js
@@ -60,5 +60,36 @@ module.exports = [
 				height: 768,
 			},
 		],
-	}
+	},
+	{
+		label: 'nci-guide-card-with-image-and-description guide-card-row',
+		storyId: 'components-nci-guide-card--guide-card-with-image-and-description',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 880,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
 ];
--- testing/ncids-css-testing/stories/components/nci-guide-card/nci-guide-card.stories.jsx
+++ testing/ncids-css-testing/stories/components/nci-guide-card/nci-guide-card.stories.jsx
@@ -3,6 +3,6 @@ export default {
 	argTypes: {},
 };
 
-
 export * from './default.stories';
 export * from './guide-card-row.stories';
+export * from './nci-guide-card-with-image-and-description.stories';
