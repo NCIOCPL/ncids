04bbbc1cd4545b51fbcd96af7fe253327e66d953
Olivia Tharp <olivia.tharp@nih.gov>
2023-03-13 15:50:57 -0500

    (#00000) fixing regressions

--- testing/ncids-css-testing/.storybook/main.js
+++ testing/ncids-css-testing/.storybook/main.js
@@ -70,4 +70,10 @@ module.exports = {
 
 		return config;
 	},
+	previewHead: (head) => (`
+	 ${head}
+   <link rel="preconnect" href="https://fonts.googleapis.com" />
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Poppins:wght@300;400;600&family=Roboto+Mono:wght@300;400;600&display=swap" rel="stylesheet" />
+  `)
 };
