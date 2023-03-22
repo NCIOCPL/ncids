c2655c1c5a1413615ec81ef23a8d12ccde0e5566
Olivia Tharp <olivia.tharp@nih.gov>
2023-03-22 16:03:49 -0500

    (#825) Change usa-skipnav story to twig

--- testing/ncids-css-testing/stories/components/usa-skipnav/usa-skipnav.stories.jsx
+++ testing/ncids-css-testing/stories/components/usa-skipnav/usa-skipnav.stories.jsx
@@ -1,6 +1,21 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import Component from './skipnav-with-content.twig';
+import css from './usa-skipnav.scss';
+
 export default {
-	title: "components/usa-skipnav",
+	title: 'components/usa-skipnav',
 	argTypes: {},
 };
 
-export * from "./default.stories";
+const Template = (args) => Component(args);
+
+// For this example we want to use the skipnav component twig, but also need
+// other content for the test. So we are making a local twig that includes
+// the shared template.
+export const Default = () => <TestCase css={css} html={Template.bind({})(
+	{
+		label: 'Skip to main content',
+		href: '#main-content',
+	}
+)} />;
