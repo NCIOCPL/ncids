03fb7ba4cdda837d55acf9abd88fdcfdd0c652cc
Olivia Tharp <olivia.tharp@nih.gov>
2023-03-27 10:09:08 -0500

    (#827) Change usa-banner story to use twig

--- testing/ncids-css-testing/stories/components/usa-banner/usa-banner.stories.jsx
+++ testing/ncids-css-testing/stories/components/usa-banner/usa-banner.stories.jsx
@@ -1,7 +1,31 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import Component from '@nciocpl/ncids-twig/components/usa-banner/usa-banner.twig';
+import css from './nci-banner.scss';
+
 export default {
 	title: "components/usa-banner",
 	argTypes: {},
 };
 
-export * from "./nci-banner.stories";
-export * from "./nci-banner-with-language-toggle.stories";
+const Template = (args) => Component(args);
+
+export const NCIBanner = () => <TestCase css={css} html={Template.bind({})(
+	{
+		language: 'en'
+	}
+)} />;
+
+export const NCIBannerWithLanguageToggle = () => <TestCase css={css} html={Template.bind({})(
+	{
+		language: 'en',
+		language_toggle_href: 'javascript:void(0);'
+	}
+)} />;
+
+export const SpanishNCIBannerWithLanguageToggle = () => <TestCase css={css} html={Template.bind({})(
+	{
+		language: 'es',
+		language_toggle_href: 'javascript:void(0);'
+	}
+)} />;
