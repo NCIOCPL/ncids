8107824e2568ab8f37739617327ef33f4c33ea4b
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2023-03-30 09:05:34 -0600

    (#833) Fixed bad html in promo block examples and stories
    Used HTML validator to fix up some bad html that made its way in.
    - removed labels from sections
    - removed alt tags from buttons and links
    - removed hrefs from buttons

--- testing/ncids-css-testing/stories/components/nci-promo-block/default.stories.jsx
+++ testing/ncids-css-testing/stories/components/nci-promo-block/default.stories.jsx
@@ -4,13 +4,18 @@ import css from './nci-promo-block.scss';
 
 // language=HTML
 const html = `
-<section label="Promoblock No Image" class="usa-section usa-section--light">
+<section class="usa-section usa-section--light">
     <div class="grid-container">
         <div class="nci-promo-block nci-promo-block--dark">
             <div class="nci-promo-block__content">
                 <h2 class="nci-promo-block__heading">NCI Annual Plan & Budget Proposal for Fiscal Year 2024</h2>
-                <p class="nci-promo-block__text">NCI's research strategy supports investigator-initiated research and maximizes opportunities in emerging areas of science. The FY 2024 Annual Plan & Budget Proposal aligns with NCI's vision and supports cancer research.</p>
-                <a class="usa-button usa-button--secondary">Learn More<span class="usa-sr-only"> about the NCI Annual Plan</span></a>
+                <p class="nci-promo-block__text">NCI's research strategy supports investigator-initiated research 
+                  and maximizes opportunities in emerging areas of science. The FY 2024 Annual Plan & Budget Proposal 
+                  aligns with NCI's vision and supports cancer research.
+                </p>
+                <a class="usa-button usa-button--secondary">
+                  Learn More<span class="usa-sr-only"> about the NCI Annual Plan</span>
+                </a>
             </div>
         </div>
     </div>
