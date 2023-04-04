f6f94ea73b39257ffbeadcef9ec622def9a9756a
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-04 09:03:50 -0500

    (#879) Change usa-list story to twig

--- testing/ncids-css-testing/stories/components/usa-list/usa-list.scenarios.js
+++ testing/ncids-css-testing/stories/components/usa-list/usa-list.scenarios.js
@@ -1,11 +1,35 @@
 module.exports = [
 	{
-		label: 'usa-list default',
-		storyId: 'components-usa-list--default',
+		label: 'usa-list ordered',
+		storyId: 'components-usa-list--ordered',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		]
+	},
+	{
+		label: 'usa-list unordered',
+		storyId: 'components-usa-list--unordered',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		]
 	},
 	{
 		label: 'usa-list unstyled',
 		storyId: 'components-usa-list--unstyled',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		]
 	},
-
 ];
--- testing/ncids-css-testing/stories/components/usa-list/usa-list.stories.jsx
+++ testing/ncids-css-testing/stories/components/usa-list/usa-list.stories.jsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import Component from '@nciocpl/ncids-twig/components/usa-list/usa-list.twig';
+import css from './usa-list.scss';
+
 export default {
 	title: 'components/usa-list',
 	argTypes: {},
 };
 
-export * from './list.stories';
-export * from './unstyled.stories';
+const Template = (args) => Component(args);
+
+export const Ordered = () => <TestCase css={css} html={Template.bind({})({ element: 'ol' })} />;
+export const Unordered = () => <TestCase css={css} html={Template.bind({})({ element: 'ul' })} />;
+export const Unstyled = () => <TestCase css={css} html={Template.bind({})({
+	element: 'ul',
+	classes: 'usa-list--unstyled',
+})} />;
