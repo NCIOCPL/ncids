952e5d412a50135bb00d45ffa452d4610990115e
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-05 11:11:09 -0500

    (#838) Add uswds alert to DS

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-alert/usa-alert.scenarios.js
@@ -0,0 +1,154 @@
+module.exports = [
+	{
+		label: 'usa-alert default',
+		storyId: 'uswds-components-alert--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert emergency',
+		storyId: 'uswds-components-alert--emergency',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert error',
+		storyId: 'uswds-components-alert--error',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert info',
+		storyId: 'uswds-components-alert--info',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert no-header',
+		storyId: 'uswds-components-alert--no-header',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert no-icon',
+		storyId: 'uswds-components-alert--no-icon',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert slim',
+		storyId: 'uswds-components-alert--slim',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert success',
+		storyId: 'uswds-components-alert--success',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+
+	{
+		label: 'usa-alert warning',
+		storyId: 'uswds-components-alert--warning',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+];
