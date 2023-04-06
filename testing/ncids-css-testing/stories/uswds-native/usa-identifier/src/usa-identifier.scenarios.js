ee4b1b76568efc28b0d3690e7cf749007a3ddffe
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-06 08:30:38 -0500

    (#860) Add usa-identifier to DS

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-identifier/src/usa-identifier.scenarios.js
@@ -0,0 +1,170 @@
+module.exports = [
+	{
+		label: 'usa-identifier default',
+		storyId: 'uswds-components-identifier--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-identifier default spanish',
+		storyId: 'uswds-components-identifier--default-spanish',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-identifier multiple parents',
+		storyId: 'uswds-components-identifier--multiple-parents-and-logos',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-identifier multiple parents spanish',
+		storyId: 'uswds-components-identifier--multiple-parents-and-logos-spanish',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-identifier no logos',
+		storyId: 'uswds-components-identifier--no-logos',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-identifier no logos spanish',
+		storyId: 'uswds-components-identifier--no-logos-spanish',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-identifier taxpayer disclaimer',
+		storyId: 'uswds-components-identifier--taxpayer-disclaimer',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-identifier taxpayer disclaimer spanish',
+		storyId: 'uswds-components-identifier--taxpayer-disclaimer-spanish',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+];
