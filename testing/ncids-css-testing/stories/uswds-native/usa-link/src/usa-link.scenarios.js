293946cbddfd403d96791ccb5a848eeb9f81ae46
karlikpj <paul.karlik@nih.gov>
2023-04-07 14:25:18 -0400

    (#862) USWDS Link Passthough

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-link/src/usa-link.scenarios.js
@@ -0,0 +1,13 @@
+module.exports = [
+	{
+		label: 'usa-link default',
+		storyId: 'uswds-components-link--link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	}
+];
