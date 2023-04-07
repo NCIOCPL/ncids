ec0e6c8f3d90d52b3ad9c2f9c4a7a97b0487cb44
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-04-07 10:58:23 -0400

    (#791) Adds light promoblock with image
    changes spans to h2

--- testing/ncids-css-testing/stories/components/nci-promo-block/nci-promo-block.scenarios.js
+++ testing/ncids-css-testing/stories/components/nci-promo-block/nci-promo-block.scenarios.js
@@ -63,6 +63,27 @@ module.exports = [
 		],
 	},
 	{
+		label: 'nci-promo-block light-with-image',
+		storyId: 'components-nci-promo-block--light-with-image',
+		viewports: [
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
 		label: 'nci-promo-block with-image-alternating',
 		storyId: 'components-nci-promo-block--with-image-alternating',
 		viewports: [
