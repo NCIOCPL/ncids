8943a9fa02aa97f93416703954596fddf806b6ca
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-04-11 11:16:24 -0400

    (#901) Add aria-labelledby attribute to promoblock

--- testing/ncids-css-testing/stories/components/nci-promo-block/light.stories.jsx
+++ testing/ncids-css-testing/stories/components/nci-promo-block/light.stories.jsx
@@ -4,11 +4,11 @@ import css from './nci-promo-block.scss';
 
 // language=HTML
 const html = `
-<section label="Promoblock No Image" class="usa-section usa-section--light">
+<section class="usa-section usa-section--light" aria-labelledby="promoblock-title">
     <div class="grid-container">
         <div class="nci-promo-block">
             <div class="nci-promo-block__content">
-                <h2 class="nci-promo-block__heading">NCI Annual Plan & Budget Proposal for Fiscal Year 2024</h2>
+                <h2 id="promoblock-title" class="nci-promo-block__heading">NCI Annual Plan & Budget Proposal for Fiscal Year 2024</h2>
                 <p class="nci-promo-block__text">NCI's research strategy supports investigator-initiated research and maximizes opportunities in emerging areas of science. The FY 2024 Annual Plan & Budget Proposal aligns with NCI's vision and supports cancer research.</p>
                 <a class="usa-button usa-button--secondary">Learn More<span class="usa-sr-only"> about the NCI Annual Plan</span></a>
             </div>
