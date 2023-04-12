4ae180bbb48cb1832d46045313d9160804f7fc5e
karlikpj <paul.karlik@nih.gov>
2023-04-12 12:14:42 -0400

    (#874) Uswds Table Component

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-table/src/usa-table--stacked/content/index.js
@@ -0,0 +1,4 @@
+export { default as StackedDefaultContent } from "./usa-table--stacked.json";
+export { default as StackedBorderlessContent } from "./usa-table--stacked~borderless.json";
+export { default as StackedHeaderBorderlessContent } from "./usa-table--stacked~header-borderless.json";
+export { default as StackedHeaderContent } from "./usa-table--stacked~header.json";
