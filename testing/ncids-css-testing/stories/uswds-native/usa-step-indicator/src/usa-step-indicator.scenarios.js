c6a4062c90d115d46ba4197b245537e0208b4997
karlikpj <paul.karlik@nih.gov>
2023-04-12 16:14:05 -0400

    (#872) Step Indicator Component

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-step-indicator/src/usa-step-indicator.scenarios.js
@@ -0,0 +1,146 @@
+module.exports = [
+	{
+		label: 'usa-step-indicator default',
+		storyId: 'uswds-components-step-indicator--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator center counters sm',
+		storyId: 'uswds-components-step-indicator--center-counters-sm',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator center counters',
+		storyId: 'uswds-components-step-indicator--center-counters',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator center no label',
+		storyId: 'uswds-components-step-indicator--center-no-label',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator center',
+		storyId: 'uswds-components-step-indicator--center',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator counters sm',
+		storyId: 'uswds-components-step-indicator--counters-sm',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator counters',
+		storyId: 'uswds-components-step-indicator--counters',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator no label',
+		storyId: 'uswds-components-step-indicator--no-label',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'usa-step-indicator short',
+		storyId: 'uswds-components-step-indicator--short',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+		],
+	},
+];
---
+++ testing/ncids-css-testing/stories/uswds-native/usa-step-indicator/src/usa-step-indicator.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TestCase } from '../../../../components/test-case';
+import Component from './usa-step-indicator.twig';
+import {
+  DefaultContent,
+  CenterCountersSmContent,
+  CenterCountersContent,
+  CenterNoLabelContent,
+  CenterContent,
+  CountersSmContent,
+  CountersContent,
+  NoLabelsContent,
+  ShortContent,
+} from './content';
+import css from './index.scss';
+
+export default {
+	title: 'USWDS/Components/Step Indicator',
+};
+
+const Template = (args) => Component(args);
+
+export const Default = () => <TestCase css={css} html={Template.bind({})(DefaultContent)} />;
+
+export const CenterCountersSm = () => <TestCase css={css} html={Template.bind({})(CenterCountersSmContent)} />;
+
+export const CenterCounters = () => <TestCase css={css} html={Template.bind({})(CenterCountersContent)} />;
+
+export const CenterNoLabel = () => <TestCase css={css} html={Template.bind({})(CenterNoLabelContent)} />;
+
+export const Center = () => <TestCase css={css} html={Template.bind({})(CenterContent)} />;
+
+export const CountersSm = () => <TestCase css={css} html={Template.bind({})(CountersSmContent)} />;
+
+export const Counters = () => <TestCase css={css} html={Template.bind({})(CountersContent)} />;
+
+export const NoLabels = () => <TestCase css={css} html={Template.bind({})(NoLabelsContent)} />;
+
+export const Short = () => <TestCase css={css} html={Template.bind({})(ShortContent)} />;
