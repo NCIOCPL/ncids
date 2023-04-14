4b38a2eb4e0607f9893454f427df8ec2810b595c
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-14 17:24:40 -0500

    (#865) Add usa-pagingation to DS

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-pagination/src/usa-pagination.scenarios.js
@@ -0,0 +1,176 @@
+module.exports = [
+	// default
+	{
+		label: 'usa-pagination default',
+		storyId: 'uswds-components-pagination--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+
+	// links
+	{
+		label: 'usa-pagination link focus',
+		storyId: 'uswds-components-pagination--default',
+		focusSelector: '.usa-pagination__link-text',
+		viewports: [
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-pagination link hover',
+		storyId: 'uswds-components-pagination--default',
+		hoverSelector: '.usa-pagination__link-text',
+		viewports: [
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-pagination link active',
+		storyId: 'uswds-components-pagination--default',
+		activeSelector: '.usa-pagination__link-text',
+		viewports: [
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+
+	// buttons
+	{
+		label: 'usa-pagination button focus',
+		storyId: 'uswds-components-pagination--default',
+		focusSelector: '.usa-pagination__button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-pagination button hover',
+		storyId: 'uswds-components-pagination--default',
+		hoverSelector: '.usa-pagination__button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-pagination button active',
+		storyId: 'uswds-components-pagination--default',
+		activeSelector: '.usa-pagination__button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+
+	// variants
+	{
+		label: 'usa-pagination unbounded',
+		storyId: 'uswds-components-pagination--unbounded',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-pagination spanish',
+		storyId: 'uswds-components-pagination--spanish',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-pagination spanish unbounded',
+		storyId: 'uswds-components-pagination--spanish-unbounded',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-pagination test',
+		storyId: 'uswds-components-pagination--test',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 360,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+		],
+	},
+];
