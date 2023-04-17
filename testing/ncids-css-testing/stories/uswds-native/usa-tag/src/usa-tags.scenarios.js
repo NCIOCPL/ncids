6feb929d70b1d100b2b42251512c6c77101af763
karlikpj <paul.karlik@nih.gov>
2023-04-17 13:16:56 -0400

    (#892) Tags Component Pass though

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-tag/src/usa-tags.scenarios.js
@@ -0,0 +1,24 @@
+module.exports = [
+	{
+		label: 'usa-tags default',
+		storyId: 'uswds-components-tags--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-tags no text',
+		storyId: 'uswds-components-tags--big',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	}
+];
