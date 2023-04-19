5b967ca023977df962a5421f9313ce7f53e4b73b
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-19 16:51:52 -0500

    (#844) Add uswds checklist to DS

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-checklist/src/usa-checklist.scenarios.js
@@ -0,0 +1,13 @@
+module.exports = [
+	{
+		label: 'usa-checklist default',
+		storyId: 'uswds-components-form-inputs-checklist--checklist',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	}
+];
