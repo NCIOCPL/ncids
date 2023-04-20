3ff69caeb2c2c1b7260d2fb07890396b169ea2f5
karlikpj <paul.karlik@nih.gov>
2023-04-20 02:24:25 -0400

    (#852) Text Input Pass though

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-input/src/usa-input.scenarios.js
@@ -0,0 +1,37 @@
+module.exports = [
+	{
+		label: 'usa-input default',
+		storyId: 'uswds-components-text-input--input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-input focus',
+		storyId: 'uswds-components-text-input--input',
+		focusSelector: '.usa-input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-input focus textarea',
+		storyId: 'uswds-components-text-input--input',
+		focusSelector: '.usa-textarea',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	}
+];
