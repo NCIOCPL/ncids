afade517a2850d8534035b1a39476b5642f30cee
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-04-20 13:51:55 -0400

    (#856) Adds usa-select input passthrough

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-select/src/usa-select.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Component from './usa-select.twig';
+import ComponentMultiple from './usa-select-multiple.twig';
+import { TestCase } from '../../../../components/test-case';
+import css from './index.scss';
+
+export default {
+  title: 'USWDS/Components/Select',
+};
+
+const Template = (args) => Component(args);
+const TemplateMultiple = (args) => ComponentMultiple(args);
+
+export const Default = () => <TestCase css={css} html={Template.bind({})()} />;
+export const MultipleSelect = () => <TestCase css={css} html={TemplateMultiple.bind({})()} />;
