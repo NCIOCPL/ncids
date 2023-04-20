7108bdd621aadf53aac7c2863332ea7bfbe94f8c
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-04-20 13:51:55 -0400

    (#856) Adds usa-select input passthrough

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-select/src/usa-select.scenarios.js
@@ -0,0 +1,60 @@
+module.exports = [
+	{
+		label: 'usa-select default',
+		storyId: 'uswds-components-select--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+    {
+		label: 'usa-select default focus',
+		storyId: 'uswds-components-select--default',
+        focusSelector: '.usa-select',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+    {
+		label: 'usa-select default click',
+		storyId: 'uswds-components-select--default',
+        clickSelector: '.usa-select',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+    {
+		label: 'usa-select multiple-select',
+		storyId: 'uswds-components-select--multiple-select',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+    {
+		label: 'usa-select multiple-select click',
+		storyId: 'uswds-components-select--multiple-select',
+        clickSelector: '.usa-select',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	}
+];
