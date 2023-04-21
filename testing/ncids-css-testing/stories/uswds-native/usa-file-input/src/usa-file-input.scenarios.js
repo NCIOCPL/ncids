df4ee7c60b0a1e8f83dcdf95167708eb516c94d4
karlikpj <paul.karlik@nih.gov>
2023-04-21 12:00:56 -0400

    (#849) File Input Pass Through
    Closes #849

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-file-input/src/usa-file-input.scenarios.js
@@ -0,0 +1,200 @@
+module.exports = [
+	{
+		label: 'usa-file-input input',
+		storyId: 'uswds-components-form-inputs-file-input--input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input input file selected',
+		storyId: 'uswds-components-form-inputs-file-input--input',
+		mockFileUpload: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input input focus',
+		storyId: 'uswds-components-form-inputs-file-input--input',
+		focusSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input input active',
+		storyId: 'uswds-components-form-inputs-file-input--input',
+		clickSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input disabled',
+		storyId: 'uswds-components-form-inputs-file-input--disabled',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input error',
+		storyId: 'uswds-components-form-inputs-file-input--error',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input error focus',
+		storyId: 'uswds-components-form-inputs-file-input--error',
+		focusSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input error active',
+		storyId: 'uswds-components-form-inputs-file-input--error',
+		clickSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input multiple',
+		storyId: 'uswds-components-form-inputs-file-input--multiple',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input multiple focus',
+		storyId: 'uswds-components-form-inputs-file-input--multiple',
+		focusSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input multiple active',
+		storyId: 'uswds-components-form-inputs-file-input--multiple',
+		clickSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input specific',
+		storyId: 'uswds-components-form-inputs-file-input--specific',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input specific focus',
+		storyId: 'uswds-components-form-inputs-file-input--specific',
+		focusSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input specific active',
+		storyId: 'uswds-components-form-inputs-file-input--specific',
+		clickSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input wildcard',
+		storyId: 'uswds-components-form-inputs-file-input--wildcard',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input wildcard focus',
+		storyId: 'uswds-components-form-inputs-file-input--wildcard',
+		focusSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-file-input wildcard active',
+		storyId: 'uswds-components-form-inputs-file-input--wildcard',
+		clickSelector: '.usa-file-input__input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	}
+];
