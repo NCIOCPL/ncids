751d669f3decdb1be7f9bcd491772ae434e0f97e
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-28 11:50:29 -0500

    (#891) Add usa-search to ncids
    Closes #891

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-search/src/usa-search.stories.js
@@ -0,0 +1,34 @@
+import Component from './usa-search.twig';
+import {
+	DefaultContent,
+	BigContent,
+	SmallContent,
+	DefaultContentLangEs,
+	BigContentLangEs,
+	SmallContentLangEs,
+} from './content';
+import css from './index.scss';
+import img from '@nciocpl/ncids-css/uswds-img/usa-icons-bg/search--white.svg';
+
+import { TestCase } from '../../../../components/test-case';
+import { search } from '@uswds-js';
+
+export default {
+	title: 'USWDS/Components/Search',
+	args: {
+		behavior: search
+	}
+};
+
+const replaceContent = (content) => {
+	const template = Template.bind({})(content)
+	return template.replace('./img/usa-icons-bg/search--white.svg', img);
+};
+
+const Template = (args) => Component(args);
+export const Default = () => <TestCase css={css} html={replaceContent(DefaultContent)} />;
+export const Big = () => <TestCase css={css} html={replaceContent(BigContent)} />;
+export const Small = () => <TestCase css={css} html={replaceContent(SmallContent)} />;
+export const DefaultLangEs = () => <TestCase css={css} html={replaceContent(DefaultContentLangEs)} />;
+export const BigLangEs = () => <TestCase css={css} html={replaceContent(BigContentLangEs)} />;
+export const SmallLangEs = () => <TestCase css={css} html={replaceContent(SmallContentLangEs)} />;
