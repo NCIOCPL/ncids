c09c93845cdf2040323b5d5d7cc7fd6906ddec7f
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-28 15:42:46 -0500

    (#859) Add usa-icon-list to ncids

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-icon-list/src/usa-icon-list.scenarios.js
@@ -0,0 +1,57 @@
+module.exports = [
+	{
+		label: 'usa-icon-list default',
+		storyId: 'uswds-components-icon-list--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-icon-list custom-rich',
+		storyId: 'uswds-components-icon-list--custom-rich',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-icon-list custom-size',
+		storyId: 'uswds-components-icon-list--custom-size',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-icon-list rich',
+		storyId: 'uswds-components-icon-list--rich',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-icon-list simple',
+		storyId: 'uswds-components-icon-list--simple',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+];
