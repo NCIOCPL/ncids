a165e7e94a9a89e26814bc94eefe61ddeefa31d2
Olivia Tharp <olivia.tharp@nih.gov>
2023-04-28 11:50:29 -0500

    (#891) Add usa-search to ncids
    Closes #891

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-search/src/usa-search.scenarios.js
@@ -0,0 +1,262 @@
+module.exports = [
+	// default
+	{
+		label: 'usa-search default',
+		storyId: 'uswds-components-search--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search default button hover',
+		storyId: 'uswds-components-search--default',
+		hoverSelector: '.usa-button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search default button focus',
+		storyId: 'uswds-components-search--default',
+		focusSelector: '.usa-button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search default input focus',
+		storyId: 'uswds-components-search--default',
+		focusSelector: '.usa-input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+
+	// big
+	{
+		label: 'usa-search big',
+		storyId: 'uswds-components-search--big',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search big button hover',
+		storyId: 'uswds-components-search--big',
+		hoverSelector: '.usa-button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search big button focus',
+		storyId: 'uswds-components-search--big',
+		focusSelector: '.usa-button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search big input focus',
+		storyId: 'uswds-components-search--big',
+		focusSelector: '.usa-input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+
+	// small
+	{
+		label: 'usa-search small',
+		storyId: 'uswds-components-search--small',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search small button hover',
+		storyId: 'uswds-components-search--small',
+		hoverSelector: '.usa-button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search small button focus',
+		storyId: 'uswds-components-search--small',
+		focusSelector: '.usa-button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-search small input focus',
+		storyId: 'uswds-components-search--small',
+		focusSelector: '.usa-input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+
+	// default es
+	{
+		label: 'usa-search default es',
+		storyId: 'uswds-components-search--default-lang-es',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+
+	// big es
+	{
+		label: 'usa-search big es',
+		storyId: 'uswds-components-search--big-lang-es',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+
+	// small es
+	{
+		label: 'usa-search small es',
+		storyId: 'uswds-components-search--small-lang-es',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+		],
+	},
+];
