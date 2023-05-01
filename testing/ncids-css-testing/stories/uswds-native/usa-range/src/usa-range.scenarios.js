bcc8eaf3501bbaa2aff4a116565637c597e85e80
Olivia Tharp <olivia.tharp@nih.gov>
2023-05-01 09:35:03 -0500

    (#855) Add usa-range to NCIDS
    Closes #855

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-range/src/usa-range.scenarios.js
@@ -0,0 +1,39 @@
+module.exports = [
+	// default
+	{
+		label: 'usa-range default',
+		storyId: 'uswds-components-form-inputs-range--range',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default input states
+	{
+		label: 'usa-range default focus',
+		storyId: 'uswds-components-form-inputs-range--range',
+		focusSelector: '.usa-range',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-range default click',
+		storyId: 'uswds-components-form-inputs-range--range',
+		clickSelector: '.usa-range',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+];
