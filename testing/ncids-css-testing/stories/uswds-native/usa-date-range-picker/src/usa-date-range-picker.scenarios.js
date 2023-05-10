fa5da47bd715366e9a440b31f0aabf0076d5f052
Olivia Tharp <olivia.tharp@nih.gov>
2023-05-10 11:44:34 -0500

    (#848) Add date ranger picker to NCIDS
    Closes #848
    
    Pass through component for Date Range Picker.

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-date-range-picker/src/usa-date-range-picker.scenarios.js
@@ -0,0 +1,144 @@
+module.exports = [
+	// default
+	{
+		label: 'usa-date-range-picker default',
+		storyId: 'uswds-components-form-inputs-date-range-picker--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default focus
+	{
+		label: 'usa-date-range-picker default focus',
+		storyId: 'uswds-components-form-inputs-date-range-picker--default',
+		focusSelector: '.usa-date-picker__external-input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default active
+	{
+		label: 'usa-date-range-picker default active',
+		storyId: 'uswds-components-form-inputs-date-range-picker--default',
+		clickSelector: '.usa-date-picker__button',
+		postInteractionWait: 150, 
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default-date
+	{
+		label: 'usa-date-range-picker default-date',
+		storyId: 'uswds-components-form-inputs-date-range-picker--default-date',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default focus
+	{
+		label: 'usa-date-range-picker default date focus',
+		storyId: 'uswds-components-form-inputs-date-range-picker--default-date',
+		focusSelector: '.usa-date-picker__external-input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default active
+	{
+		label: 'usa-date-range-picker default date active',
+		storyId: 'uswds-components-form-inputs-date-range-picker--default-date',
+		clickSelector: '.usa-date-picker__button',
+		postInteractionWait: 350, 
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// restricted-date
+	{
+		label: 'usa-date-range-picker restricted-date',
+		storyId: 'uswds-components-form-inputs-date-range-picker--restricted-date',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default focus
+	{
+		label: 'usa-date-range-picker restricted-date focus',
+		storyId: 'uswds-components-form-inputs-date-range-picker--restricted-date',
+		focusSelector: '.usa-date-picker__external-input',
+		postInteractionWait: 350, 
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// default active
+	{
+		label: 'usa-date-range-picker restricted-date active',
+		storyId: 'uswds-components-form-inputs-date-range-picker--restricted-date',
+		clickSelector: '.usa-date-picker__button',
+		postInteractionWait: 150, 
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// disabled
+	{
+		label: 'usa-date-range-picker disabled',
+		storyId: 'uswds-components-form-inputs-date-range-picker--disabled',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	// aria-disabled
+	{
+		label: 'usa-date-range-picker aria-disabled',
+		storyId: 'uswds-components-form-inputs-date-range-picker--aria-disabled',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+];
