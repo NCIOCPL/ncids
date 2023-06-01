fb58d0c00a2dbf3ce219960860982691f0fce3bc
karlikpj <paul.karlik@nih.gov>
2023-06-01 09:50:53 -0400

    (#853) Memorable Date
    Closes #853
    
    Pass though component for Memorable Date

---
+++ testing/ncids-css-testing/stories/uswds-native/usa-memorable-date/src/usa-memorable-date.scenarios.js
@@ -0,0 +1,37 @@
+module.exports = [
+	{
+		label: 'usa-memoriable-date default',
+		storyId: 'uswds-components-form-inputs-memorable-date--memorable-date',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-memoriable-date default focus input',
+		storyId: 'uswds-components-form-inputs-memorable-date--memorable-date',
+		focusSelector: '.usa-input',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-memoriable-date default focus select',
+		storyId: 'uswds-components-form-inputs-memorable-date--memorable-date',
+		focusSelector: '.usa-select',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	}
+];
