0d57d3d2de8f24bb284129ad622438a25c8290ad
karlikpj <paul.karlik@nih.gov>
2023-06-02 13:29:57 -0400

    (#1013) Remove Extra Classes from Examples
    Closes #1013
    
    Removed usa-section--nci-cta-strip classes and bad aria tags.

--- testing/ncids-css-testing/stories/components/nci-cta-strip/default-spanish.stories.jsx
+++ testing/ncids-css-testing/stories/components/nci-cta-strip/default-spanish.stories.jsx
@@ -4,9 +4,9 @@ import css from './nci-cta-strip.scss';
 
 // language=HTML
 const html = `
-<section class="usa-section usa-section--dark usa-section--nci-cta-strip">
+<section class="usa-section usa-section--dark">
 	<div class="grid-container">
-		<ul class="nci-cta-strip" aria-label="Llamada a la acción">
+		<ul class="nci-cta-strip">
 			<li><a href="http://www.cancer.gov" class="usa-button">El NCI para la Nanotecnología en el Cáncer</a></li>
 			<li><a href="http://www.google.com/" class="usa-button">Laboratorio de Caracterización de Nanotecnología</a></li>
 			<li><a href="http://www.cancer.gov" class="usa-button">Plan de Nanotecnología del Cáncer</a></li>
