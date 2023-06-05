ef2a44d980cb9168dbe4a5b5c6975f3ca2f9faa4
karlikpj <paul.karlik@nih.gov>
2023-06-05 12:52:11 -0400

    (#1007) Remove Utility Classes from Examples
    Closes #1007
    
    Remove utility classes from example pages and doc site.

--- testing/ncids-css-testing/stories/components/nci-hero/default.stories.jsx
+++ testing/ncids-css-testing/stories/components/nci-hero/default.stories.jsx
@@ -12,7 +12,7 @@ import img_hero_widerscreen from './img/hero-widescreen.jpg';
 // language=HTML
 const html = `
 <section aria-labelledby="nci-hero-text">
-    <div class="nci-hero text-center">
+    <div class="nci-hero">
         <picture class="nci-hero__image">
             <source media="(min-width: 1024px)" srcset="${img_hero_widerscreen}" />
             <source media="(min-width: 880px)" srcset="${img_hero_desktop}" />
@@ -22,7 +22,7 @@ const html = `
             <img src="${img_hero_mobile}" alt="" />
         </picture>
         <div class="nci-hero__cta-container">
-            <div class="nci-hero__cta padding-6">
+            <div class="nci-hero__cta">
                 <h2 class="nci-hero__cta-tagline" id="nci-hero-text">
                     NCI is the nation's leader in cancer research.
                 </h2>
