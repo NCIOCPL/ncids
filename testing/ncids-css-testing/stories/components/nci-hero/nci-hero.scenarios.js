b60773b3f46f70106e21387524604fc671abe3d2
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2023-07-19 11:13:00 -0400

    (#793, #792) NCI Hero with Dark Tagline and Button
    Closes #793
    Closes #792

--- testing/ncids-css-testing/stories/components/nci-hero/nci-hero.scenarios.js
+++ testing/ncids-css-testing/stories/components/nci-hero/nci-hero.scenarios.js
@@ -153,5 +153,67 @@ module.exports = [
 				height: 768,
 			},
 		],
+	},
+	{
+		label: 'nci-hero dark',
+		storyId: 'components-nci-hero--dark',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-hero dark-with-button',
+		storyId: 'components-nci-hero--dark-with-button',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
 	}
 ];
