e7738d262e0d62f0ab4a4e7d9a7a352f9b8ee4d4
Olivia Tharp <olivia.tharp@nih.gov>
2023-08-21 11:34:59 -0500

    (#1107) Create nci header mobile menu default source
    Closes #1107

---
+++ packages/ncids-js/src/components/nci-header/__tests__/mobile-menu/nci-header.mobile-menu-default-source.test.ts
@@ -0,0 +1,51 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import { fireEvent, screen } from '@testing-library/dom';
+
+import { NCIExtendedHeaderWithMegaMenu } from '../../nci-header.component';
+import { MockMegaMenuAdaptor } from '../mega-menu/mega-menu-adaptor.mock';
+import { DefaultMobileMenuSource } from '../../utils/mobile-menu/default-mobile-menu-source';
+import { headerWithDataMenuId } from '../nci-header-id-dom';
+
+describe('NCI Extended Header - Mobile Menu Default Source', () => {
+	beforeEach(() => {
+		Object.defineProperty(window, 'matchMedia', {
+			writable: true,
+			value: jest.fn().mockImplementation((query) => ({
+				matches: query === '(min-width: 1024px)',
+				media: query,
+				onchange: null,
+				addListener: jest.fn(), // Deprecated
+				removeListener: jest.fn(), // Deprecated
+				addEventListener: jest.fn(),
+				removeEventListener: jest.fn(),
+				dispatchEvent: jest.fn(),
+			})),
+		});
+	});
+
+	afterEach(() => {
+		document.getElementsByTagName('body')[0].innerHTML = '';
+		jest.restoreAllMocks();
+	});
+
+	it('should render primary items from primary nav', async () => {
+		global.innerWidth = 600;
+		const container = headerWithDataMenuId();
+		document.body.append(container);
+
+		const element = document.getElementById('nci-header');
+		NCIExtendedHeaderWithMegaMenu.create(<HTMLElement>element, {
+			megaMenuSource: new MockMegaMenuAdaptor(true),
+			mobileMenuSource: new DefaultMobileMenuSource(),
+		});
+
+		const button = await screen.findByText('Menu');
+		expect(button).toBeVisible();
+		fireEvent.click(button);
+
+		const query1 = await screen.findByText('First section');
+		expect(query1).toBeInTheDocument();
+	});
+});
