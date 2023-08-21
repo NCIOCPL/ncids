0c34d99c856dc114cf57dd751b33eb8f204bfc89
Olivia Tharp <olivia.tharp@nih.gov>
2023-08-21 11:34:59 -0500

    (#1107) Create nci header mobile menu default source
    Closes #1107

---
+++ packages/ncids-js/src/components/nci-header/utils/mobile-menu/default-mobile-menu-source.ts
@@ -0,0 +1,86 @@
+import { MobileMenuAdaptor } from './mobile-menu-adaptor';
+import { MobileMenuData } from './mobile-menu-data';
+import { MobileMenuItem } from './mobile-menu-item';
+
+/**
+ * Default header mobile menu source
+ */
+export class DefaultMobileMenuSource implements MobileMenuAdaptor {
+	/** Use href for navigation id. */
+	useUrlForNavigationId = true;
+	/** List of navigation items from the primary navigation. */
+	primaryNavItems: HTMLElement[];
+	/** Use href for navigation id. */
+	items: MobileMenuItem[];
+	/** Nav item language code. */
+	lang: string;
+
+	constructor() {
+		// Inherit mobile menu language from html.
+		this.lang = document.documentElement.lang;
+
+		// Create array from node list.
+		this.primaryNavItems = Array.from(
+			document.querySelectorAll('.nci-header-nav__primary-item a')
+		);
+
+		// Fallback menu item.
+		this.items = [
+			{
+				id: 0,
+				label: 'Error',
+				path: '/',
+				langcode: this.lang,
+				hasChildren: false,
+			},
+		];
+	}
+
+	/**
+	 * Gets initial menu id.
+	 * @return {Promise<string | number>}
+	 */
+	async getInitialMenuId(): Promise<string | number> {
+		return 0;
+	}
+
+	/**
+	 * Gets mobile nav structure.
+	 * @param {number | string} id
+	 * @return {Promise<MobileMenuData>}
+	 */
+	async getNavigationLevel(id: number | string): Promise<MobileMenuData> {
+		return this.createDefaultMobileMenu(id);
+	}
+
+	/**
+	 * Creates mobile menu from primary nav items.
+	 * @param {string | number} id
+	 * @return {MobileMenuData}
+	 */
+	createDefaultMobileMenu(id: string | number): MobileMenuData {
+		this.items = this.primaryNavItems.map((item, index) => {
+			const anchor = item as HTMLAnchorElement;
+			const path = anchor.href;
+			const textContent = item.textContent as string;
+
+			return {
+				id: index,
+				label: textContent,
+				path: path,
+				langcode: this.lang,
+				hasChildren: false,
+			};
+		});
+
+		return {
+			id: id,
+			label: '',
+			path: '/',
+			langcode: this.lang,
+			hasChildren: true,
+			items: this.items,
+			parent: null,
+		};
+	}
+}
