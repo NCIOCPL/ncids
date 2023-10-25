706efd85ed7ab0daa9b558ff2b6a2b56b4552db0
Adrian Cofie <adriancofie@gmail.com>
2023-10-25 09:54:52 -0400

    (#1236) Add Storybook to ncids/react
    * Adds storybook to the ncids-react package
    
    Closes #1236

--- packages/ncids-react/.eslintrc.js
+++ packages/ncids-react/.eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-	extends: [
-		'@nciocpl/eslint-config-react'
-	],
+	extends: ["@nciocpl/eslint-config-react", "plugin:storybook/recommended"],
 	// Plugins are configured by the recommended extensions above
 	rules: {
 		"react/display-name": "off",
