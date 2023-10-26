3745f1cf51fe98377a1a0d067ee8eb1f6d6e3641
karlikpj <paul.karlik@nih.gov>
2023-10-26 17:36:03 -0400

    (#1363) Missing 404 Page
    Closes #1363

---
+++ docs/src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error404Page = () => {
+	return (
+		<div className="usa-section padding-2">
+			<h1>404 Page not found</h1>
+		</div>
+	);
+};
+
+export default Error404Page;
