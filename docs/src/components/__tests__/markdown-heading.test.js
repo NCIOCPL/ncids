9668b73ffe08219a1aa3ec5e057caeadb0926a0e
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2023-11-01 07:25:35 -0600

    (#1357) update header links on doc site
    Closes #1357
    
    - updated component template to handle header linkage
    - updated layout for links on non-component pages
    - improved accessibility for the header link (KISS)

--- docs/src/components/__tests__/markdown-heading.test.js
+++ docs/src/components/__tests__/markdown-heading.test.js
@@ -1,32 +1,16 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 
 import MarkdownHeader from '../markdown-heading';
 
 describe('Markdown Header', () => {
-	it('renders an h1 with anchor', async () => {
-		const H1 = MarkdownHeader(1);
-
-		render(<H1>This is my title</H1>);
-
-		expect(
-			screen.getByText('This is my title', { selector: 'h1' })
-		).toBeInTheDocument();
-		expect(
-			screen.getByLabelText('This is my title permalink')
-		).toBeInTheDocument();
-	});
-
 	it('renders an h2 with anchor', async () => {
 		const H2 = MarkdownHeader(2);
 
 		render(<H2>This is my title</H2>);
 
 		expect(
-			screen.getByText('This is my title', { selector: 'h2' })
-		).toBeInTheDocument();
-		expect(
-			screen.getByLabelText('This is my title permalink')
+			screen.getByText('This is my title', { selector: 'h2 a' })
 		).toBeInTheDocument();
 	});
 
@@ -36,10 +20,7 @@ describe('Markdown Header', () => {
 		render(<H3>This is my title</H3>);
 
 		expect(
-			screen.getByText('This is my title', { selector: 'h3' })
-		).toBeInTheDocument();
-		expect(
-			screen.getByLabelText('This is my title permalink')
+			screen.getByText('This is my title', { selector: 'h3 a' })
 		).toBeInTheDocument();
 	});
 
@@ -49,10 +30,7 @@ describe('Markdown Header', () => {
 		render(<H4>This is my title</H4>);
 
 		expect(
-			screen.getByText('This is my title', { selector: 'h4' })
-		).toBeInTheDocument();
-		expect(
-			screen.getByLabelText('This is my title permalink')
+			screen.getByText('This is my title', { selector: 'h4 a' })
 		).toBeInTheDocument();
 	});
 
@@ -62,10 +40,7 @@ describe('Markdown Header', () => {
 		render(<H5>This is my title</H5>);
 
 		expect(
-			screen.getByText('This is my title', { selector: 'h5' })
-		).toBeInTheDocument();
-		expect(
-			screen.getByLabelText('This is my title permalink')
+			screen.getByText('This is my title', { selector: 'h5 a' })
 		).toBeInTheDocument();
 	});
 
@@ -75,10 +50,7 @@ describe('Markdown Header', () => {
 		render(<H6>This is my title</H6>);
 
 		expect(
-			screen.getByText('This is my title', { selector: 'h6' })
-		).toBeInTheDocument();
-		expect(
-			screen.getByLabelText('This is my title permalink')
+			screen.getByText('This is my title', { selector: 'h6 a' })
 		).toBeInTheDocument();
 	});
 
@@ -91,17 +63,7 @@ describe('Markdown Header', () => {
 			</H6>
 		);
 
-		expect(
-			screen.getByLabelText('This is my title permalink')
-		).toBeInTheDocument();
-	});
-
-	it('Handles empty text', async () => {
-		const H6 = MarkdownHeader(6);
-
-		render(<H6 />);
-
-		expect(screen.getByLabelText('permalink')).toBeInTheDocument();
+		expect(screen.getByText('This is my title')).toBeInTheDocument();
 	});
 
 	it('Throws error on unknown level', async () => {
