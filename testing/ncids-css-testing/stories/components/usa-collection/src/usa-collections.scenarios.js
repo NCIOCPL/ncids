7c73d14aca2401a9d1545c774bf0e321ace6e35b
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2023-11-27 15:49:59 -0500

    (#1149) NCIDS Collections
    - Added public assests to storybook config to allow
    file path to be passed through json
    
    Closes #1149

---
+++ testing/ncids-css-testing/stories/components/usa-collection/src/usa-collections.scenarios.js
@@ -0,0 +1,90 @@
+module.exports = [
+	{
+		label: 'usa-collection default',
+		storyId: 'components-collection-default--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-collection calendar',
+		storyId: 'components-collection-default--calendar',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-collection media',
+		storyId: 'components-collection-default--media',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-collection only headers',
+		storyId: 'components-collection-variants--only-headers',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-collection media image visible',
+		storyId: 'components-collection-default--media',
+		viewports: [
+			{
+				label: 'desktop',
+				width: 880,
+				height: 300,
+			},
+		],
+	},
+	{
+		label: 'usa-collection media image not visible',
+		storyId: 'components-collection-default--media',
+		viewports: [
+			{
+				label: 'tablet-lg',
+				width: 879,
+				height: 280,
+			},
+		],
+	},
+	{
+		label: 'usa-collection calendar desktop size',
+		storyId: 'components-collection-default--calendar',
+		viewports: [
+			{
+				label: 'desktop',
+				width: 880,
+				height: 200,
+			},
+		],
+	},
+	{
+		label: 'usa-collection calendar mobile size',
+		storyId: 'components-collection-default--calendar',
+		viewports: [
+			{
+				label: 'tablet-lg',
+				width: 879,
+				height: 200,
+			},
+		],
+	}
+];
