6808490f27c704509c16d063adf6426254238421
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2023-11-27 14:14:59 -0500

    (#580) Adds styles for buttons with icons
    Closes #580

---
+++ testing/ncids-css-testing/stories/components/usa-button/icon-buttons.js
@@ -0,0 +1,289 @@
+const iconSvg = `
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	width="24"
+	class="usa-icon"
+	role="img"
+	aria-labelledby="facebook-title_site">
+	<title id="facebook-title_site">Facebook</title>
+	<rect fill="none" height="24" width="24" />
+	<path d="M22,12c0-5.52-4.48-10-10-10S2,6.48,2,12c0,4.84,3.44,8.87,8,9.8V15H8v-3h2V9.5C10,7.57,11.57,6,13.5,6H16v3h-2 c-0.55,0-1,0.45-1,1v2h3v3h-3v6.95C18.05,21.45,22,17.19,22,12z" />
+</svg>
+`;
+
+export const defaultButtonsIconLeft = `
+<button class="usa-button usa-button--nci-icon">
+	${iconSvg}
+	Icon Left
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--hover">
+	${iconSvg}
+	Hover
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--active">
+	${iconSvg}
+	Active
+</button>
+<button class="usa-button usa-button--nci-icon usa-focus">
+	${iconSvg}
+	Focus
+</button>
+<button class="usa-button usa-button--nci-icon" disabled>
+	${iconSvg}
+	Disabled
+</button>
+`;
+
+export const outlineButtonsIconLeft = `
+<button class="usa-button usa-button--nci-icon usa-button--outline">
+	${iconSvg}
+	Outline Left
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-button--hover">
+	${iconSvg}
+	Hover
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-button--active">
+	${iconSvg}
+	Active
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-focus">
+	${iconSvg}
+	Focus
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline" disabled>
+	${iconSvg}
+	Disabled
+</button>
+`;
+
+export const secondaryOutlineButtonsIconLeft = `
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-button--secondary">
+	${iconSvg}
+	Outline Secondary Left
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-button--secondary usa-button--hover">
+	${iconSvg}
+	Hover
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-button--secondary usa-button--active">
+	${iconSvg}
+	Active
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-button--secondary usa-focus">
+	${iconSvg}
+	Focus
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--outline usa-button--secondary" disabled>
+	${iconSvg}
+	Disabled
+</button>
+`;
+
+export const defaultButtonsIconRight = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right">
+	Icon Right
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--hover">
+	Hover
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--active">
+	Active
+	${iconSvg}
+	</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-focus">
+	Focus
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right" disabled>
+	Disabled
+	${iconSvg}
+</button>
+`;
+
+export const outlineButtonsIconRight = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline">
+	Outline Right
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-button--hover">
+	Hover
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-button--active">
+	Active
+	${iconSvg}
+	</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-focus">
+	Focus
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline" disabled>
+	Disabled
+	${iconSvg}
+</button>
+`;
+
+export const secondaryOutlineButtonsIconRight = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-button--secondary">
+	Outline Secondary Right
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-button--secondary usa-button--hover">
+	Hover
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-button--secondary usa-button--active">
+	Active
+	${iconSvg}
+	</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-button--secondary usa-focus">
+	Focus
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--outline usa-button--secondary" disabled>
+	Disabled
+	${iconSvg}
+</button>
+`;
+
+export const defaultFullWidthIconButtonLeft = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width">
+	${iconSvg}
+	Icon Button Left
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--hover">
+	${iconSvg}
+	Hover
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--active">
+	${iconSvg}
+	Active
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-focus">
+	${iconSvg}
+	Focus
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width" disabled>
+	${iconSvg}
+	Disabled
+</button>
+`;
+
+export const outlineFullWidthIconButtonLeft = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline">
+	${iconSvg}
+	Outline Left
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-button--hover">
+	${iconSvg}
+	Hover
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-button--active">
+	${iconSvg}
+	Active
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-focus">
+	${iconSvg}
+	Focus
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline" disabled>
+	${iconSvg}
+	Disabled
+</button>
+`;
+
+export const secondaryOutlineFullWidthIconButtonLeft = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-button--secondary">
+	${iconSvg}
+	Secondary Outline Left
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-button--secondary usa-button--hover">
+	${iconSvg}
+	Hover
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-button--secondary usa-button--active">
+	${iconSvg}
+	Active
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-button--secondary usa-focus">
+	${iconSvg}
+	Focus
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-full-width usa-button--outline usa-button--secondary" disabled>
+	${iconSvg}
+	Disabled
+</button>
+`;
+
+export const defaultFullWidthIconButtonRight = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width">
+	Icon Button Right
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--hover">
+	Hover
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--active">
+	Active
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-focus">
+	Focus
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width" disabled>
+	Disabled
+	${iconSvg}
+</button>
+`;
+
+export const outlineFullWidthIconButtonRight = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline">
+	Outline Right
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-button--hover">
+	Hover
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-button--active">
+	Active
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-focus">
+	Focus
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline" disabled>
+	Disabled
+	${iconSvg}
+</button>
+`;
+
+export const secondaryOutlineFullWidthIconButtonRight = `
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-button--secondary">
+	Secondary Outline Right
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-button--secondary usa-button--hover">
+	Hover
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-button--secondary usa-button--active">
+	Active
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-button--secondary usa-focus">
+	Focus
+	${iconSvg}
+</button>
+<button class="usa-button usa-button--nci-icon usa-button--nci-icon-right usa-button--nci-full-width usa-button--outline usa-button--secondary" disabled>
+	Disabled
+	${iconSvg}
+</button>
+`;
