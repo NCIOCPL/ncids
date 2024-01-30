9956ec391571f6cbf471c8844d5aae24c38c6f47
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2024-01-30 13:50:27 -0500

    (#1423) Adds gatsby-ssr file for JS on mdx pages
    Closes #1423

---
+++ docs/gatsby-ssr.js
@@ -0,0 +1,8 @@
+
+// Note: The APIs wrapPageElement and wrapRootElement exist in both
+// the browser and Server-Side Rendering (SSR) APIs. You generally should
+// implement the same components in both gatsby-ssr.js and gatsby-browser.js
+// so that pages generated through SSR are the same after being hydrated in the browser.
+// Gatsby Docs: https://v3.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
+
+export { default as wrapRootElement } from './src/components/layouts/wrap-root-element';
