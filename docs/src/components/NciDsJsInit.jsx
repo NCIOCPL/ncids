d7b4822c8b16b87df4313ae3899c15a53877c8c1
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2024-01-30 13:56:35 -0500

    (#1497) Adds gatsby-ssr file for mdx file JS
    Closes #1497

--- docs/src/components/NciDsJsInit.jsx
+++ docs/src/components/NciDsJsInit.jsx
@@ -2,14 +2,33 @@ import React from 'react';
 import PropType from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-const NciDsJsInit = ({ children }) => {
+// The code below is meant to be run on Static HTML to initialize
+// ncids-js components in a <code> preview block. However, even when
+// statically published, the Gatsby app is still a React app on the
+// client side that re-hydrates the page. When a link produced by
+// gatsby-link is clicked on within the Gatsby app, it does not just
+// take the user to another page, but pre-fetches the page and replaces
+// the contents of _gatsby. Thus the window object is never cleared out.
+// So the code below tries to manage that we do not create multiple
+// listeners for the same page if a user navigates away and then back
+// to the page. It also makes sure we limit the number of event
+// listeners.
+const NciDsJsInit = ({ path, children }) => {
 	const html = `
+		window.ncidsPreviewHandlers = window.ncidsPreviewHandlers || {};
+	  if (!window.ncidsPreviewHandlers['${path}']) {
 			const handler = (preview) => {
 				${children}
-			}
-			window.addEventListener('NCIDS:Preview', (evt) => {
-				handler(evt.target);
-			})
+		  }
+	  }
+    if (!window.ncidsPreviewListenerRegistered) {
+		  window.addEventListener('NCIDS:Preview', (evt) => {
+        for(const [, handler] of Object.entries(window.ncidsPreviewHandlers)) {
+		      handler(evt.target);
+        }
+		  });
+      window.ncidsPreviewListenerRegistered = true;
+    }
 	`;
 	return (
 		<Helmet>
@@ -19,6 +38,7 @@ const NciDsJsInit = ({ children }) => {
 };
 
 NciDsJsInit.propTypes = {
+	path: PropType.string,
 	children: PropType.string,
 };
 
