89cfeea2ed7cfffb688ec0fb3a738b438b0abf16
Dev Rana <dev-rana-publicis@users.noreply.github.com>
2024-02-09 16:11:17 -0500

    (#1398) Patch usa-summary-box
    Closes #1398

---
+++ testing/ncids-css-testing/stories/components/usa-summary-box/src/usa-summary-box.scenarios.js
@@ -0,0 +1,106 @@
+module.exports = [
+	{
+		label: 'usa-summary-box default',
+		storyId: 'components-summary-box-default--default',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-summary-box default hover',
+		storyId: 'components-summary-box-default--default',
+		hoverSelector: '.usa-summary-box__link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-summary-box default focus',
+		storyId: 'components-summary-box-default--default',
+		focusSelector: '.usa-summary-box__link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-summary-box default active',
+		storyId: 'components-summary-box-default--default',
+		activeSelector: '.usa-summary-box__link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-summary-box row',
+		storyId: 'components-summary-box-test-cases--summary-box-row',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 740,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 520,
+			},
+			{
+				label: 'tablet-lg',
+				width: 880,
+				height: 380,
+			},
+		],
+	},
+	{
+		label: 'usa-summary-box row hover',
+		storyId: 'components-summary-box-test-cases--summary-box-row',
+		hoverSelector: '.usa-summary-box__link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 740,
+			},
+		],
+	},
+	{
+		label: 'usa-summary-box row focus',
+		storyId: 'components-summary-box-test-cases--summary-box-row',
+		focusSelector: '.usa-summary-box__link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 740,
+			},
+		],
+	},
+	{
+		label: 'usa-summary-box row active',
+		storyId: 'components-summary-box-test-cases--summary-box-row',
+		activeSelector: '.usa-summary-box__link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 740,
+			},
+		],
+	},
+];
