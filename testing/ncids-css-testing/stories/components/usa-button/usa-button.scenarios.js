396a131ec87ac02c127ea9218f4abe36e0a2d08c
karlikpj <paul.karlik@nih.gov>
2024-03-21 13:38:35 -0400

    (#1580) Update Storybook Buttons
    - Updated twigjs to fix issue with macros in loops
    
    Closes #1580

--- testing/ncids-css-testing/stories/components/usa-button/usa-button.scenarios.js
+++ testing/ncids-css-testing/stories/components/usa-button/usa-button.scenarios.js
@@ -1,73 +1,139 @@
+const viewports = [
+	{
+		label: 'mobile',
+		width: 479,
+		height: 360,
+	},		
+	{
+		label: 'tablet-lg',
+		width: 880,
+		height: 360,
+	},
+];
+
+const viewports_lg = [
+	{
+		label: 'mobile',
+		width: 479,
+		height: 360,
+	},			
+	{
+		label: 'mobile-lg',
+		width: 480,
+		height: 360,
+	},
+	{
+		label: 'tablet-lg',
+		width: 880,
+		height: 360,
+	},
+];
+
 module.exports = [
+	// Default
 	{
-		label: 'usa-button',
-		storyId: 'components-usa-button--button',
+		label: 'usa-button default button',
+		storyId: 'components-button-default--button',
+		viewports: viewports,
 	},
 	{
-		label: 'usa-button accent-cool',
-		storyId: 'components-usa-button--accent-cool',
+		label: 'usa-button default anchor',
+		storyId: 'components-button-default--anchor',
+		viewports: viewports,
+	},
+
+	// Variants
+	// Outline
+	{
+		label: 'usa-button outline button',
+		storyId: 'components-button-variants-outline--button',
+		viewports: viewports,
 	},
 	{
-		label: 'usa-button accent-warm',
-		storyId: 'components-usa-button--accent-warm',
+		label: 'usa-button outline anchor',
+		storyId: 'components-button-variants-outline--anchor',
+		viewports: viewports,
+	},
+
+	// Unstyled
+	{
+		label: 'usa-button unstyled button',
+		storyId: 'components-button-variants-unstyled--button',
+		viewports: viewports_lg,
 	},
 	{
-		label: 'usa-button base',
-		storyId: 'components-usa-button--base',
+		label: 'usa-button unstyled anchor',
+		storyId: 'components-button-variants-unstyled--anchor',
+		viewports: viewports_lg,
+	},
+
+	// Modifiers
+	// Big
+	{
+		label: 'usa-button big button',
+		storyId: 'components-button-modifiers-size-big--button',
+		viewports: viewports,
 	},
 	{
-		label: 'usa-button big',
-		storyId: 'components-usa-button--big',
+		label: 'usa-button big anchor',
+		storyId: 'components-button-modifiers-size-big--anchor',
+		viewports: viewports,
+	},
+
+	// Small
+	{
+		label: 'usa-button small button',
+		storyId: 'components-button-modifiers-size-small--button',
+		viewports: viewports,
 	},
 	{
-		label: 'usa-button nci-small',
-		storyId: 'components-usa-button--nci-small',
+		label: 'usa-button small anchor',
+		storyId: 'components-button-modifiers-size-small--anchor',
+		viewports: viewports,
+	},
+
+	// Full width
+	{
+		label: 'usa-button full-width button',
+		storyId: 'components-button-modifiers-full-width--anchor',
+		viewports: viewports,
 	},
 	{
-		label: 'usa-button outline',
-		storyId: 'components-usa-button--outline',
+		label: 'usa-button full-width anchor',
+		storyId: 'components-button-modifiers-full-width--button',
+		viewports: viewports,
+	},
+
+	// Icons
+	// Icon, left
+	{
+		label: 'usa-button icon-left button',
+		storyId: 'components-button-modifiers-icon-icon-left--button',
+		viewports: viewports_lg,
 	},
 	{
-		label: 'usa-button outline-inverse',
-		storyId: 'components-usa-button--outline-inverse',
+		label: 'usa-button icon-left anchor',
+		storyId: 'components-button-modifiers-icon-icon-left--anchor',
+		viewports: viewports_lg,
+	},
+
+	// Icon, right
+	{
+		label: 'usa-button icon-right button',
+		storyId: 'components-button-modifiers-icon-icon-right--button',
+		viewports: viewports_lg,
 	},
 	{
-		label: 'usa-button outline-secondary',
-		storyId: 'components-usa-button--outline-secondary',
+		label: 'usa-button icon-right anchor',
+		storyId: 'components-button-modifiers-icon-icon-right--anchor',
+		viewports: viewports_lg,
 	},
+
+	// Test Cases
+	// Unsupported
 	{
-		label: 'usa-button secondary',
-		storyId: 'components-usa-button--secondary',
-	},
-	{
-		label: 'usa-button unstyled',
-		storyId: 'components-usa-button--unstyled',
-	},
-	{
-		label: 'usa-button icon-button',
-		storyId: 'components-usa-button--icon-button',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 360,
-				height: 360,
-			},
-			{
-				label: 'mobile-lg',
-				width: 480,
-				height: 360,
-			},
-		]
-	},
-	{
-		label: 'usa-button nci-full-width',
-		storyId: 'components-usa-button--nci-full-width',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 360,
-				height: 360,
-			}
-		]
+		label: 'usa-button unsupported',
+		storyId: 'components-button-test-cases-unsupported--outline-colors',
+		viewports: viewports,
 	},
 ];
