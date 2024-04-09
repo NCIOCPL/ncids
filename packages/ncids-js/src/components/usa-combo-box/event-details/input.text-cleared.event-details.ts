3e436a51979d974b23444b862bc2693e9f062b5e
Olivia Tharp <olivia.tharp@nih.gov>
2024-04-09 12:01:11 -0500

    (#1642) Update combo box text clear/unselected event
    - Updates some lacking documentation as well
    
    Closes #1642

---
+++ packages/ncids-js/src/components/usa-combo-box/event-details/input.text-cleared.event-details.ts
@@ -0,0 +1,16 @@
+import { ComboBoxEventDetails } from './combo-box.event-details';
+
+/**
+ * Custom event details for the `combo-box:input:text-cleared` event.
+ */
+export type ComboBoxTextClearedEventDetails = ComboBoxEventDetails & {
+	/**
+	 * An
+	 * [HTML collection of selected options](https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/selectedOptions)
+	 * via the combo box select element. May be empty if no value was selected
+	 * before the text was cleared.
+	 */
+	selected: HTMLCollectionOf<HTMLOptionElement>;
+	/** The previous value of the combo box input before the text was cleared. */
+	previousInputValue: string;
+};
