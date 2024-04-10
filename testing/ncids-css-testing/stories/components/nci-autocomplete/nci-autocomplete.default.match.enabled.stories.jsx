6c1bf830c7d1fd8a4d9ccfc6209f8b2af8e95597
karlikpj <paul.karlik@nih.gov>
2024-04-10 15:43:17 -0400

    (#1577) Autocomplete Storybook Update
    - Added engine script for handling blur so that we can test
      keyPresses then unfocus the element.
    
    Closes #1577

---
+++ testing/ncids-css-testing/stories/components/nci-autocomplete/nci-autocomplete.default.match.enabled.stories.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TestCase } from '../../../components/test-case';
+import Component from './nci-autocomplete.twig';
+import css from './nci-autocomplete.scss';
+import { NCIAutocomplete } from '@nciocpl/ncids-js';
+import autocompleteSource from './autocomplete-source';
+
+export default {
+	title: 'components/Autocomplete/Default',
+	args: {
+		ncidsInitJs: () => {
+			const acInput = document.getElementById('myInput');
+			NCIAutocomplete.create(acInput, {
+				highlightMatchingText: true,
+				autocompleteSource: {
+					getAutocompleteSuggestions: autocompleteSource,
+				},
+				maxOptionsCount: 10,
+				minCharCount: 3,
+				listboxClasses: 'listboxWidth',
+				minPlaceholderMsg: 'Enter 3 or more characters',
+			});
+		},
+	},
+};
+
+const Template = (args) => Component(args);
+
+export const MatchEnabled = () => (
+	<TestCase
+		css={css}
+		html={Template.bind({})({
+			label: 'Search',
+			button: 'Submit',
+		})}
+	/>
+);
--- testing/ncids-css-testing/stories/components/nci-autocomplete/nci-autocomplete.scenarios.js
+++ testing/ncids-css-testing/stories/components/nci-autocomplete/nci-autocomplete.scenarios.js
@@ -1,26 +1,202 @@
+const scenarios = ['match-disabled', 'match-enabled'].reduce(
+	(acc, matchSetting) => {
+		return [
+			...acc,
+			// Initial State
+			{
+				label: `nci-autocomplete default ${matchSetting} initial`,
+				storyId: `components-autocomplete-default--${matchSetting}`,
+				viewports: [
+					{
+						label: 'mobile',
+						width: 479,
+						height: 360,
+					},
+				],
+			},
+			// Less than 3 characters text appearing
+			{
+				label: `nci-autocomplete default ${matchSetting} below-min`,
+				storyId: `components-autocomplete-default--${matchSetting}`,
+				keyPressSelectors: [
+					{
+						selector: '#myInput',
+						keyPress: 'bi',
+					},
+				],
+				viewports: [
+					{
+						label: 'mobile',
+						width: 479,
+						height: 360,
+					},
+				],
+			},
+			// Matches appearing
+			{
+				label: `nci-autocomplete default ${matchSetting} matches showing`,
+				storyId: `components-autocomplete-default--${matchSetting}`,
+				keyPressSelectors: [
+					{
+						selector: '#myInput',
+						keyPress: 'bile',
+					},
+				],
+				viewports: [
+					{
+						label: 'mobile',
+						width: 479,
+						height: 360,
+					},
+				],
+			},
+			// Matches hovering
+			{
+				label: `nci-autocomplete default ${matchSetting} option-highlighted`,
+				storyId: `components-autocomplete-default--${matchSetting}`,
+				keyPressSelectors: [
+					{
+						selector: '#myInput',
+						keyPress: 'bile',
+					},
+				],
+				delay: 100,
+				hoverSelector: '#myInput-terms .nci-autocomplete__option:nth-child(2)',
+				viewports: [
+					{
+						label: 'mobile',
+						width: 479,
+						height: 360,
+					},
+				],
+			},
+			// Test X button with no matches and focus
+			{
+				label: `nci-autocomplete default ${matchSetting} no-match with focus`,
+				storyId: `components-autocomplete-default--${matchSetting}`,
+				keyPressSelectors: [
+					{
+						selector: '#myInput',
+						keyPress: 'chicken',
+					},
+				],
+				viewports: [
+					{
+						label: 'mobile',
+						width: 479,
+						height: 360,
+					},
+				],
+			},
+			// Test no X button with no matches without focus
+			{
+				label: `nci-autocomplete default ${matchSetting} no-match without focus`,
+				storyId: `components-autocomplete-default--${matchSetting}`,
+				keyPressSelectors: [
+					{
+						selector: '#myInput',
+						keyPress: 'chicken',
+					},
+				],
+				blurSelector: '#myInput',
+				viewports: [
+					{
+						label: 'mobile',
+						width: 479,
+						height: 360,
+					},
+				],
+			},
+			// Test X button with no matches with hover
+			{
+				label: `nci-autocomplete default ${matchSetting} no-match with hover`,
+				storyId: `components-autocomplete-default--${matchSetting}`,
+				keyPressSelectors: [
+					{
+						selector: '#myInput',
+						keyPress: 'chicken',
+					},
+				],
+				blurSelector: '#myInput',
+				hoverSelector: '#myInput',
+				viewports: [
+					{
+						label: 'mobile',
+						width: 479,
+						height: 360,
+					},
+				],
+			},
+		];
+	},
+	[]
+);
+
 module.exports = [
+	...scenarios,
 	{
-		label: 'nci-autocomplete default',
-		storyId: 'components-nci-autocomplete--default',
+		label: 'nci-autocomplete test-cases no-js',
+		storyId: 'components-autocomplete-test-cases--no-js',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
 	},
 	{
-		label: 'nci-autocomplete input-below-min-threshold',
-		storyId: 'components-nci-autocomplete--below-min',
+		label: 'nci-autocomplete test-cases no-js focus',
+		storyId: 'components-autocomplete-test-cases--no-js',
+		keyPressSelectors: [
+			{
+				selector: '#myInput',
+				keyPress: 'bile',
+			},
+		],
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
 	},
 	{
-		label: 'nci-autocomplete input-below-min-message',
-		storyId: 'components-nci-autocomplete--min-msg',
+		label: 'nci-autocomplete test-cases no-js with content blur',
+		storyId: 'components-autocomplete-test-cases--no-js',
+		keyPressSelectors: [
+			{
+				selector: '#myInput',
+				keyPress: 'bile',
+			},
+		],
+		blurSelector: '#myInput',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
 	},
 	{
-		label: 'nci-autocomplete suggestions',
-		storyId: 'components-nci-autocomplete--suggestions',
-	},
-	{
-		label: 'nci-autocomplete option-highlighted',
-		storyId: 'components-nci-autocomplete--option-highlighted',
-	},
-	{
-		label: 'nci-autocomplete matching-highlight-disabled',
-		storyId: 'components-nci-autocomplete--match-disabled',
+		label: 'nci-autocomplete test-cases no-js with content hover',
+		storyId: 'components-autocomplete-test-cases--no-js',
+		keyPressSelectors: [
+			{
+				selector: '#myInput',
+				keyPress: 'bile',
+			},
+		],
+		blurSelector: '#myInput',
+		hoverSelector: '#myInput',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
 	},
 ];
