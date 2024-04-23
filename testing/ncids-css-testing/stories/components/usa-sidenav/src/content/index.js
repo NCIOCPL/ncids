420ed9bd5915b35ca16ff76de05fc915f9cf9264
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2024-04-23 13:23:04 -0400

    (#1591) Updates usa-sidenav storybook content
    Closes #1591

---
+++ testing/ncids-css-testing/stories/components/usa-sidenav/src/content/index.js
@@ -0,0 +1,3 @@
+export { default as DefaultContent } from './usa-sidenav--default.json';
+export { default as LongContent } from './usa-sidenav--long.json';
+export { default as FullContent } from './usa-sidenav--full.json';
