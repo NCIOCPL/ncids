cb75c65f31e2cf9b8ce06f580f616f4a60cb8f0d
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2024-04-29 10:35:58 -0400

    (#1584) Updates storybook structure for usa-footer
    - Adds langcode support to story test cases
    - Adds mouse scroll amount for handling scrollwheel to engine
      scripts. scrollToView was not firing off scroll events.
    
    Closes #1584

---
+++ testing/ncids-css-testing/stories/components/usa-footer/content/index.js
@@ -0,0 +1,3 @@
+// export { NciBigFooterContent, NciBigFooterContentSpanish } from "./usa-footer--nci-big";
+export { default as NciBigFooterContent } from "./usa-footer--nci-big.json";
+export { default as NciBigFooterContentSpanish } from "./usa-footer--nci-big--spanish.json";
