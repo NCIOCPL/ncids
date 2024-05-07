2c536ebd0ec0c1cd60fffd3213c392a51a896fc0
karlikpj <paul.karlik@nih.gov>
2024-05-07 14:18:11 -0400

    (#1595) Skipnav Storybook Update
    Closes #1595

--- testing/ncids-css-testing/stories/components/usa-skipnav/usa-skipnav.stories.jsx
+++ testing/ncids-css-testing/stories/components/usa-skipnav/usa-skipnav.stories.jsx
@@ -4,7 +4,7 @@ import Component from './skipnav-with-content.twig';
 import css from './usa-skipnav.scss';
 
 export default {
-	title: 'components/usa-skipnav',
+	title: 'components/Skipnav/Default',
 	argTypes: {},
 };
 
@@ -13,9 +13,20 @@ const Template = (args) => Component(args);
 // For this example we want to use the skipnav component twig, but also need
 // other content for the test. So we are making a local twig that includes
 // the shared template.
-export const Default = () => <TestCase css={css} html={Template.bind({})(
+export const English = () => <TestCase css={css} html={Template.bind({})(
 	{
 		label: 'Skip to main content',
 		href: '#main-content',
+		placeholder: 'Navigation Placeholder',
+		maincontent: 'Main Page Content',
+	}
+)} />;
+
+export const Spanish = () => <TestCase langcode='es' css={css} html={Template.bind({})(
+	{
+		label: 'Saltar al contenido principal',
+		href: '#main-content',
+		placeholder: 'Marcador de posición de navegación',
+		maincontent: 'Contenido de la página de inicio',
 	}
 )} />;
