60806e6363afb815113474b91a58d608c3835f06
karlikpj <paul.karlik@nih.gov>
2024-05-07 14:18:11 -0400

    (#1595) Skipnav Storybook Update
    Closes #1595

--- testing/ncids-css-testing/stories/components/usa-skipnav/usa-skipnav.scenarios.js
+++ testing/ncids-css-testing/stories/components/usa-skipnav/usa-skipnav.scenarios.js
@@ -1,12 +1,50 @@
 module.exports = [
 	{
-		label: "usa-skipnav English unfocused",
-		storyId: 'components-usa-skipnav--default'
+		label: 'usa-skipnav English unfocused',
+		storyId: 'components-skipnav-default--english',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
 	},
 	{
-		label: "usa-skipnav English focused",
-		storyId: `components-usa-skipnav--default`,
+		label: 'usa-skipnav English focused',
+		storyId: 'components-skipnav-default--english',
 		focusSelector: '.usa-skipnav',
 		postInteractionWait: 200,
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-skipnav Spanish unfocused',
+		storyId: 'components-skipnav-default--spanish',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
+	},
+	{
+		label: 'usa-skipnav Spanish focused',
+		storyId: 'components-skipnav-default--spanish',
+		focusSelector: '.usa-skipnav',
+		postInteractionWait: 200,
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+		],
 	},
 ];
