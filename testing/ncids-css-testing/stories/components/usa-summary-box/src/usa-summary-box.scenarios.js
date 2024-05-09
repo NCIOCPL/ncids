9d47e6056fad2666efba6d8f66b53f422c540530
karlikpj <paul.karlik@nih.gov>
2024-05-09 16:39:20 -0400

    (#1592) Summarybox Storybook Update
    Closes #1592

--- testing/ncids-css-testing/stories/components/usa-summary-box/src/usa-summary-box.scenarios.js
+++ testing/ncids-css-testing/stories/components/usa-summary-box/src/usa-summary-box.scenarios.js
@@ -11,42 +11,6 @@ module.exports = [
 		],
 	},
 	{
-		label: 'usa-summary-box default hover',
-		storyId: 'components-summary-box-default--default',
-		hoverSelector: '.usa-summary-box__link',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 479,
-				height: 360,
-			},
-		],
-	},
-	{
-		label: 'usa-summary-box default focus',
-		storyId: 'components-summary-box-default--default',
-		focusSelector: '.usa-summary-box__link',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 479,
-				height: 360,
-			},
-		],
-	},
-	{
-		label: 'usa-summary-box default active',
-		storyId: 'components-summary-box-default--default',
-		activeSelector: '.usa-summary-box__link',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 479,
-				height: 360,
-			},
-		],
-	},
-	{
 		label: 'usa-summary-box row',
 		storyId: 'components-summary-box-test-cases--summary-box-row',
 		viewports: [
@@ -64,42 +28,6 @@ module.exports = [
 				label: 'tablet-lg',
 				width: 880,
 				height: 380,
-			},
-		],
-	},
-	{
-		label: 'usa-summary-box row hover',
-		storyId: 'components-summary-box-test-cases--summary-box-row',
-		hoverSelector: '.usa-summary-box__link',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 479,
-				height: 740,
-			},
-		],
-	},
-	{
-		label: 'usa-summary-box row focus',
-		storyId: 'components-summary-box-test-cases--summary-box-row',
-		focusSelector: '.usa-summary-box__link',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 479,
-				height: 740,
-			},
-		],
-	},
-	{
-		label: 'usa-summary-box row active',
-		storyId: 'components-summary-box-test-cases--summary-box-row',
-		activeSelector: '.usa-summary-box__link',
-		viewports: [
-			{
-				label: 'mobile',
-				width: 479,
-				height: 740,
 			},
 		],
 	},
