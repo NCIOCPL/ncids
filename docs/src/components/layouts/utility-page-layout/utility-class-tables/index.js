0b391f89b89306dbd5afdc570715ca405906938d
karlikpj <paul.karlik@nih.gov>
2024-06-07 15:30:20 -0400

    (#1667) Update Text Styles Utility Page
    Closes #1667

--- docs/src/components/layouts/utility-page-layout/utility-class-tables/index.js
+++ docs/src/components/layouts/utility-page-layout/utility-class-tables/index.js
@@ -1,3 +1,4 @@
 export * as TestUtilityClassTable from './test-utility-class-table';
 export * as TextDemonstration from './text-demonstration';
+export * as TextUnderlineColorDemonstration from './text-underline-color-demonstration';
 export * as ColorDemonstration from './color-demonstration';
