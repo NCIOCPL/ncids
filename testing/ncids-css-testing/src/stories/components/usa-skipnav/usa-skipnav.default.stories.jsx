05b2e6b2e89645318c19e4aa8174a9ecd8bea613
Olivia Tharp <olivia.tharp@nih.gov>
2024-10-14 10:16:29 -0500

    summary box, skipnav
    - TODO summary box will need to be looked at, bind has updated

--- testing/ncids-css-testing/src/stories/components/usa-skipnav/usa-skipnav.default.stories.jsx
+++ testing/ncids-css-testing/src/stories/components/usa-skipnav/usa-skipnav.default.stories.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import Component from './skipnav-with-content.twig';
-import css from './usa-skipnav.scss';
+import Component from './content/skipnav-with-content.twig';
+import css from './index.scss?inline';
 
 export default {
-	title: 'components/Skipnav/Default',
-	argTypes: {},
+	title: 'Components/Skipnav/Default',
+	component: Component,
+	parameters: { css },
 };
-
-const Template = (args) => Component(args);
 
 // For this example we want to use the skipnav component twig, but also need
 // other content for the test. So we are making a local twig that includes
 // the shared template.
-export const English = () => <TestCase css={css} html={Template.bind({})(
-	{
+export const English = {
+	args: {
 		label: 'Skip to main content',
 		href: '#main-content',
 		placeholder: 'Navigation Placeholder',
 		maincontent: 'Main Page Content',
 	}
-)} />;
+};
 
-export const Spanish = () => <TestCase langcode='es' css={css} html={Template.bind({})(
-	{
+export const Spanish = {
+	args: {
 		label: 'Saltar al contenido principal',
 		href: '#main-content',
 		placeholder: 'Marcador de posición de navegación',
 		maincontent: 'Contenido de la página de inicio',
 	}
-)} />;
+};
