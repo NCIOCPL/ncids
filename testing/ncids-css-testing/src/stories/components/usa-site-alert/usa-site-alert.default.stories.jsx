0f3a8bb0629c955e25393cdd1b2d773478e9d28a
Olivia Tharp <olivia.tharp@nih.gov>
2024-10-17 14:51:54 -0500

    site alert

---
+++ testing/ncids-css-testing/src/stories/components/usa-site-alert/usa-site-alert.default.stories.jsx
@@ -0,0 +1,17 @@
+import Component from '@nciocpl/ncids-twig/components/usa-site-alert/usa-site-alert.twig';
+import css from './index.scss?inline';
+
+import { DefaultContent } from './content/index.js';
+import { USASiteAlert } from '@nciocpl/ncids-js/usa-site-alert';
+
+export default {
+	title: 'Components/Site Alert/Default',
+	component: Component,
+	parameters: {
+		ncidsInitJs: () => USASiteAlert.createAll(),
+		css,
+	},
+};
+
+export const Default = { args: DefaultContent };
+export const DefaultDismissible = { args: { ...DefaultContent, dismissible: true } };
