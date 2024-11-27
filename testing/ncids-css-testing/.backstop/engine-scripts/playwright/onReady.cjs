5415bbc64fd909c8982b4b0e808b9293c74115c7
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2024-11-27 16:31:54 -0500

    Wait for network requests to be done before starting tests.

--- testing/ncids-css-testing/.backstop/engine-scripts/playwright/onReady.cjs
+++ testing/ncids-css-testing/.backstop/engine-scripts/playwright/onReady.cjs
@@ -6,6 +6,10 @@ module.exports = async (
 	browserContext
 ) => {
 	console.log('SCENARIO > ' + scenario.label);
+	// Wait until there are no more than network connections for at least 500ms
+	// TODO: See if adding an element to our React testing component with a waitfor selector here would be better.
+	await page.waitForLoadState('networkidle');
+
 	await require('./clickAndHoverHelper.cjs')(page, scenario);
   await require('./overrideCSS.cjs')(page, scenario);
 
