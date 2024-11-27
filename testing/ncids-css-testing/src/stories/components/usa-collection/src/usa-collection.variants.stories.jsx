9999a007a57783f991a02195d0f249cabb90a06b
Olivia Tharp <olivia.tharp@nih.gov>
2024-11-27 14:23:26 -0600

    fix broken collections img test - Fixed bad usa-collection twig

--- testing/ncids-css-testing/src/stories/components/usa-collection/src/usa-collection.variants.stories.jsx
+++ testing/ncids-css-testing/src/stories/components/usa-collection/src/usa-collection.variants.stories.jsx
@@ -1,13 +1,8 @@
+import Component from '@nciocpl/ncids-twig/components/usa-collection/usa-collection.twig';
 import css from './index.scss?inline';
-import img from '@nciocpl/ncids-css/uswds-img/sprite.svg'
-import html from '@nciocpl/ncids-twig/components/usa-collection/usa-collection.twig?twig';
+import img from '@nciocpl/ncids-css/uswds-img/sprite.svg';
 
 import { CondensedContent } from './content';
-
-const Component = () => {
-	const replaced = html().replaceAll('./img/sprite.svg', img);
-	return <div dangerouslySetInnerHTML={{ __html: replaced }}></div>;
-};
 
 export default {
 	title: 'Components/Collection/Variants',
@@ -15,4 +10,4 @@ export default {
 	parameters: { css },
 };
 
-export const Condensed = { args: CondensedContent };
+export const Condensed = { args: { ...CondensedContent, img } };
