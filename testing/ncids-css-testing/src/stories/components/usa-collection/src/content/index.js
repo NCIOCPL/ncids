f5169729750520913a7bc2c53834e735c3b69d94
Derek Lescarbeau <derek.lescarbeau@publicissapient.com>
2024-12-18 12:29:31 -0500

    (#1902) Adds automatic external link to usa-link
    Closes #1902

--- testing/ncids-css-testing/src/stories/components/usa-collection/src/content/index.js
+++ testing/ncids-css-testing/src/stories/components/usa-collection/src/content/index.js
@@ -2,3 +2,4 @@ export { default as DefaultContent } from "./usa-collection.json";
 export { default as MediaContent } from "./usa-collection~media.json";
 export { default as CalendarContent } from "./usa-collection~calendar.json";
 export { default as CondensedContent } from "./usa-collection~condensed.json";
+export { default as ExternalContent } from "./usa-collection~external.json";
--- testing/ncids-css-testing/src/stories/components/usa-collection/src/usa-collection.variants.stories.jsx
+++ testing/ncids-css-testing/src/stories/components/usa-collection/src/usa-collection.variants.stories.jsx
@@ -2,7 +2,7 @@ import Component from '@nciocpl/ncids-twig/components/usa-collection/usa-collect
 import css from './index.scss?inline';
 import img from '@nciocpl/ncids-css/uswds-img/sprite.svg';
 
-import { CondensedContent } from './content';
+import { CondensedContent, ExternalContent } from './content';
 
 export default {
 	title: 'Components/Collection/Variants',
@@ -11,3 +11,4 @@ export default {
 };
 
 export const Condensed = { args: { ...CondensedContent, img } };
+export const External = { args: { ...ExternalContent, img } };
