c1b1cde125aee89e9fc83c319a429430ed968b88
karlikpj <paul.karlik@nih.gov>
2025-01-21 09:45:42 -0500

    (#1880) Fix Pagination Icons in Storybook
    Closes #1880

--- testing/ncids-css-testing/src/stories/uswds-native/usa-pagination/src/usa-pagination.test.stories.jsx
+++ testing/ncids-css-testing/src/stories/uswds-native/usa-pagination/src/usa-pagination.test.stories.jsx
@@ -1,5 +1,6 @@
 import Component from './test/test-patterns/test-pagination-all.twig';
 import css from './index.scss?inline';
+import img_path from '@nciocpl/ncids-css/uswds-img/sprite.svg';
 
 export default {
 	title: 'USWDS/Components/Pagination',
@@ -7,4 +8,4 @@ export default {
 	parameters: { css },
 };
 
-export const Test = {};
+export const Test = { args: { img_path } };
