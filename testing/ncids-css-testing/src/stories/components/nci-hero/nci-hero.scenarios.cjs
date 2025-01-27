d97b1e68cf00c4a3fd852590f33bb767639f408a
karlikpj <paul.karlik@nih.gov>
2025-01-27 15:07:07 -0500

    (#1945) Hero Primary CTA External Link
    Closes #1945

--- testing/ncids-css-testing/src/stories/components/nci-hero/nci-hero.scenarios.cjs
+++ testing/ncids-css-testing/src/stories/components/nci-hero/nci-hero.scenarios.cjs
@@ -145,7 +145,7 @@ module.exports = [
 	},
 	{
 		label: 'nci-hero test-cases with-cta-spanish',
-		storyId: 'components-hero-test-cases-with-cta--with-cta-spanish',
+		storyId: 'components-hero-variants--with-cta-spanish',
 		viewports: [
 			{
 				label: 'mobile',
@@ -253,7 +253,115 @@ module.exports = [
 	},
 	{
 		label: 'nci-hero with-cta external-links',
-		storyId: 'components-hero-test-cases-with-cta--with-cta-external-links',
+		storyId: 'components-hero-variants--with-cta-external-links',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 880,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-hero with-cta with-external-link',
+		storyId: 'components-hero-test-cases-with-cta--with-cta-with-external-link',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 880,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-hero with-cta with-internal-override',
+		storyId: 'components-hero-test-cases-with-cta--with-cta-with-internal-link-override',
+		viewports: [
+			{
+				label: 'mobile',
+				width: 479,
+				height: 360,
+			},
+			{
+				label: 'mobile-lg',
+				width: 480,
+				height: 360,
+			},
+			{
+				label: 'tablet',
+				width: 640,
+				height: 360,
+			},
+			{
+				label: 'tablet-lg',
+				width: 880,
+				height: 360,
+			},
+			{
+				label: 'desktop',
+				width: 1024,
+				height: 768,
+			},
+			{
+				label: 'widescreen',
+				width: 1400,
+				height: 768,
+			},
+		],
+	},
+	{
+		label: 'nci-hero test-case with-cta with-external-override',
+		storyId: 'components-hero-test-cases-with-cta--with-cta-with-external-override',
 		viewports: [
 			{
 				label: 'mobile',
