3386aed782141b1c8ce7988b655bc66fb4d0eb42
karlikpj <paul.karlik@nih.gov>
2025-12-09 10:44:54 -0500

    (#2098) Accordion No Nesting Headers
    Closes #2098

--- testing/ncids-css-testing/src/stories/templates/blog-posts/default.stories.jsx
+++ testing/ncids-css-testing/src/stories/templates/blog-posts/default.stories.jsx
@@ -162,3 +162,178 @@ export const BlogSeries = {
 		secondary,
 	},
 };
+
+export const BlogSeriesCategory = {
+	args: {
+		series: {
+			title: 'Cancer Risk - Cancer Currents Blog',
+			field_intro_text:
+				'News about research on factors that affect cancer risk, including inherited genetic mutations, smoking/tobacco use, obesity, and infectious agents like HPV.',
+			body: bodyContent,
+			categories: [
+				{ name: 'Biology of Cancer', isSelected: false },
+				{ name: 'Cancer Risk', isSelected: true },
+				{ name: 'Childhood Cancer', isSelected: false },
+				{ name: 'Clinical Trail Results', isSelected: false },
+				{ name: 'Disparities', isSelected: false },
+				{ name: 'FDA Approval', isSelected: false },
+				{ name: 'Global Health', isSelected: false },
+				{ name: 'Leadership & Expert Views', isSelected: false },
+				{ name: 'Prevention', isSelected: false },
+				{ name: 'Prognosis', isSelected: false },
+				{ name: 'Screening & Early Detection', isSelected: false },
+				{ name: 'Survivorship & Supportive Care', isSelected: false },
+				{ name: 'Technology', isSelected: false },
+				{
+					name: 'Test For a Really Long Title or Category Name Here',
+					isSelected: false,
+				},
+				{ name: 'Treatment', isSelected: false },
+			],
+			archive: [
+				{ date: '2025', total: '25', isSelected: true },
+				{ date: '2024', total: '18', isSelected: false },
+				{ date: '2023', total: '22', isSelected: false },
+				{ date: '2022', total: '15', isSelected: false },
+				{ date: '2021', total: '12', isSelected: false },
+			],
+			collection: [
+				{
+					title:
+						'As Rates of Some Cancers Increase in Younger People, Researchers Search for Answers',
+					field_author: 'By Carmen Phillips',
+					field_date_posted: 'May 14, 2025',
+					field_about_blog:
+						'As diagnoses of colorectal, breast, and other cancers continue to climb in people under age 50, researchers are trying to understand what’s behind this phenomenon. Is it environmental exposures, genetics, lifestyle? The culprits, they say, remain unclear.',
+				},
+				{
+					title: 'Rapid Genetic Test Could Help Guide Brain Cancer Surgery',
+					field_author: 'By Edward Winstead',
+					field_date_posted: 'May 8, 2025',
+					field_about_blog:
+						'Scientists have developed a test for use during brain cancer surgery that rapidly measures the levels of certain genetic mutations in patients’ tumor samples. The test uses droplet digital polymerase chain reaction technology and produces results within 15 minutes.',
+				},
+				{
+					title: 'Will This Cancer Metastasize? Check Its “Stickiness”',
+					field_author: 'By Melissa Madden',
+					field_date_posted: 'May 6, 2025',
+					field_about_blog:
+						'A device that measures the “stickiness” of cancer cells in tumor samples may help predict the likelihood of a patient’s cancer metastasizing. Researchers believe the device could eventually help doctors make more informed treatment choices.',
+				},
+				{
+					title:
+						'FDA Approval Likely to Change Initial Treatment for Some People with Advanced Colorectal Cancer',
+					field_author: 'By Carmen Phillips',
+					field_date_posted: 'May 2, 2025',
+					field_about_blog:
+						'FDA has approved the combination of the immunotherapy drugs ipilimumab (Yervoy) and nivolumab (Opdivo) for the initial treatment of people with advanced colorectal cancer whose tumors are classified as MSI-H or dMMR.',
+				},
+				{
+					title:
+						'As Rates of Some Cancers Increase in Younger People, Researchers Search for Answers',
+					field_author: 'By Carmen Phillips',
+					field_date_posted: 'May 14, 2025',
+					field_about_blog:
+						'As diagnoses of colorectal, breast, and other cancers continue to climb in people under age 50, researchers are trying to understand what’s behind this phenomenon. Is it environmental exposures, genetics, lifestyle? The culprits, they say, remain unclear.',
+				},
+				{
+					title: 'Rapid Genetic Test Could Help Guide Brain Cancer Surgery',
+					field_author: 'By Edward Winstead',
+					field_date_posted: 'May 8, 2025',
+					field_about_blog:
+						'Scientists have developed a test for use during brain cancer surgery that rapidly measures the levels of certain genetic mutations in patients’ tumor samples. The test uses droplet digital polymerase chain reaction technology and produces results within 15 minutes.',
+				},
+			],
+		},
+		nci_img_logo: nciImgLogo,
+		mega_menu: primaryNojs,
+		secondary,
+	},
+};
+
+export const BlogSeriesArchive = {
+	args: {
+		series: {
+			title: '2025 - Cancer Currents Blog',
+			body: bodyContent,
+			categories: [
+				{ name: 'Biology of Cancer', isSelected: false },
+				{ name: 'Cancer Risk', isSelected: false },
+				{ name: 'Childhood Cancer', isSelected: false },
+				{ name: 'Clinical Trail Results', isSelected: false },
+				{ name: 'Disparities', isSelected: false },
+				{ name: 'FDA Approval', isSelected: false },
+				{ name: 'Global Health', isSelected: false },
+				{ name: 'Leadership & Expert Views', isSelected: false },
+				{ name: 'Prevention', isSelected: false },
+				{ name: 'Prognosis', isSelected: false },
+				{ name: 'Screening & Early Detection', isSelected: false },
+				{ name: 'Survivorship & Supportive Care', isSelected: false },
+				{ name: 'Technology', isSelected: false },
+				{
+					name: 'Test For a Really Long Title or Category Name Here',
+					isSelected: false,
+				},
+				{ name: 'Treatment', isSelected: false },
+			],
+			archive: [
+				{ date: '2025', total: '25', isSelected: true },
+				{ date: '2024', total: '18', isSelected: false },
+				{ date: '2023', total: '22', isSelected: false },
+				{ date: '2022', total: '15', isSelected: false },
+				{ date: '2021', total: '12', isSelected: false },
+			],
+			collection: [
+				{
+					title:
+						'As Rates of Some Cancers Increase in Younger People, Researchers Search for Answers',
+					field_author: 'By Carmen Phillips',
+					field_date_posted: 'May 14, 2025',
+					field_about_blog:
+						'As diagnoses of colorectal, breast, and other cancers continue to climb in people under age 50, researchers are trying to understand what’s behind this phenomenon. Is it environmental exposures, genetics, lifestyle? The culprits, they say, remain unclear.',
+				},
+				{
+					title: 'Rapid Genetic Test Could Help Guide Brain Cancer Surgery',
+					field_author: 'By Edward Winstead',
+					field_date_posted: 'May 8, 2025',
+					field_about_blog:
+						'Scientists have developed a test for use during brain cancer surgery that rapidly measures the levels of certain genetic mutations in patients’ tumor samples. The test uses droplet digital polymerase chain reaction technology and produces results within 15 minutes.',
+				},
+				{
+					title: 'Will This Cancer Metastasize? Check Its “Stickiness”',
+					field_author: 'By Melissa Madden',
+					field_date_posted: 'May 6, 2025',
+					field_about_blog:
+						'A device that measures the “stickiness” of cancer cells in tumor samples may help predict the likelihood of a patient’s cancer metastasizing. Researchers believe the device could eventually help doctors make more informed treatment choices.',
+				},
+				{
+					title:
+						'FDA Approval Likely to Change Initial Treatment for Some People with Advanced Colorectal Cancer',
+					field_author: 'By Carmen Phillips',
+					field_date_posted: 'May 2, 2025',
+					field_about_blog:
+						'FDA has approved the combination of the immunotherapy drugs ipilimumab (Yervoy) and nivolumab (Opdivo) for the initial treatment of people with advanced colorectal cancer whose tumors are classified as MSI-H or dMMR.',
+				},
+				{
+					title:
+						'As Rates of Some Cancers Increase in Younger People, Researchers Search for Answers',
+					field_author: 'By Carmen Phillips',
+					field_date_posted: 'May 14, 2025',
+					field_about_blog:
+						'As diagnoses of colorectal, breast, and other cancers continue to climb in people under age 50, researchers are trying to understand what’s behind this phenomenon. Is it environmental exposures, genetics, lifestyle? The culprits, they say, remain unclear.',
+				},
+				{
+					title: 'Rapid Genetic Test Could Help Guide Brain Cancer Surgery',
+					field_author: 'By Edward Winstead',
+					field_date_posted: 'May 8, 2025',
+					field_about_blog:
+						'Scientists have developed a test for use during brain cancer surgery that rapidly measures the levels of certain genetic mutations in patients’ tumor samples. The test uses droplet digital polymerase chain reaction technology and produces results within 15 minutes.',
+				},
+			],
+		},
+		nci_img_logo: nciImgLogo,
+		mega_menu: primaryNojs,
+		secondary,
+	},
+};
+
